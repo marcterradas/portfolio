8b8646c5f9b81bec434a0dad8c547fb39c8f59e6
Marc Terradas <terradas.marc@gmail.com>
2021-06-22 18:20:31 +0200

    add footer

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,21 +2,25 @@
     <div id="app">
         <Nav></Nav>
         <router-view></router-view>
+        <Footer></Footer>
     </div>
 </template>
 
 <script>
 import Nav from '@/components/template/Nav'
+import Footer from '@/components/template/Footer'
 
 export default {
     name: 'App',
     components: {
-        Nav
+        Nav,
+        Footer
     }
 }
 </script>
 
 <style lang="scss">
 #app {
+    // position: relative;
 }
 </style>
---
+++ app/src/components/template/Footer.vue
@@ -0,0 +1,44 @@
+<template>
+    <footer id="footer">
+        <div class="element">
+            <a href="https://github.com/marcterradas" target="_blank"> <img src="../../../public/img/github.svg" /> </a>
+        </div>
+        <div class="element">
+            <a href="https://www.linkedin.com/in/marc-terradas-zapata/" target="_blank"> <img src="../../../public/img/linkedin.svg" /> </a>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+#footer {
+    position: absolute;
+    bottom: 0;
+    height: 60px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-shadow: 0px 1px 5px 3px rgba(0, 0, 0, 0.15);
+
+    .element {
+        width: 50%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+
+        img {
+            width: 40px;
+            height: 40px;
+        }
+    }
+
+    @media screen and (max-width: 1200px) {
+        position: inherit;
+    }
+}
+</style>
