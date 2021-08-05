dbefe936aea93ea6655215b44760ba4bf10ce3ba
Marc Terradas <terradas.marc@gmail.com>
2021-08-05 12:55:42 +0200

    update education page

--- app/src/views/Education.vue
+++ app/src/views/Education.vue
@@ -1,5 +1,22 @@
 <template>
-    <div id="education">{{ $t('education') }}</div>
+    <div id="education">
+        <div class="elementContainer">
+            <div class="element">
+                <div class="degree">{{ $t('education_description.daw.degree') }}</div>
+                <div class="place">{{ $t('education_description.daw.place') }}</div>
+                <div class="duration">{{ $t('education_description.daw.duration') }}</div>
+                <div class="description" v-html="$t('education_description.daw.description')"></div>
+            </div>
+        </div>
+        <div class="elementContainer">
+            <div class="element">
+                <div class="degree">{{ $t('education_description.smx.degree') }}</div>
+                <div class="place">{{ $t('education_description.smx.place') }}</div>
+                <div class="duration">{{ $t('education_description.smx.duration') }}</div>
+                <div class="description" v-html="$t('education_description.smx.description')"></div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -12,5 +29,50 @@ export default {
 
 <style lang="scss">
 #education {
+    padding: 40px;
+
+    .elementContainer {
+        width: 100%;
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+
+        .element {
+            border-radius: 4px;
+            padding: 20px;
+            box-shadow: 0px 4px 5px 1px rgba(0, 0, 0, 0.15);
+            background-color: $white;
+            margin-bottom: 20px;
+            width: 450px;
+            display: block;
+
+            .degree {
+                font-family: Roboto-BoldItalic;
+                font-size: 18px;
+                width: 100%;
+                margin-bottom: 16px;
+            }
+
+            .place {
+                font-family: Roboto-Italic;
+                font-size: 14px;
+            }
+
+            .duration {
+                font-family: Roboto-Italic;
+                font-size: 14px;
+                margin-bottom: 10px;
+            }
+        }
+    }
+
+    @media screen and (max-width: 720px) {
+        .elementContainer {
+            .element {
+                width: 100%;
+            }
+        }
+    }
 }
 </style>
