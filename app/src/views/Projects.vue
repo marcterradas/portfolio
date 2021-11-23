05c465af6e66bd30744c78a5010804642f1c2f6a
marcterradas <terradas.marc@gmail.com>
2021-11-23 19:28:11 +0100

    init projects descriptions

--- app/src/views/Projects.vue
+++ app/src/views/Projects.vue
@@ -3,14 +3,14 @@
         <div class="project">
             <div class="title">GitHub Pages Deployer</div>
             <div class="url">https://github.com/marcterradas/githubpages-deployer</div>
-            <div class="tools"><b>Vue.js</b>,<b>JavaScript</b>,<b>SCSS</b>,<b>HTML</b></div>
-            <div class="description"></div>
+            <div class="tools"><b>Vue.js</b>,<b>Vite.js</b>,<b>JavaScript</b>,<b>SCSS</b>,<b>HTML</b></div>
+            <div class="description">{{ $t('projects.deployer.description') }}</div>
         </div>
         <div class="project">
             <div class="title">Portfolio</div>
             <div class="url">https://github.com/marcterradas/portfolio</div>
             <div class="tools"><b>Node.js</b>,<b>TypeScript</b>,<b>JEST</b></div>
-            <div class="description"></div>
+            <div class="description">{{ $t('projects.portfolio.description') }}</div>
         </div>
     </div>
 </template>
