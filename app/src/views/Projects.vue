f3f4f2515043bdc5684d431a659248ee71d937da
marcterradas <terradas.marc@gmail.com>
2021-11-23 19:42:53 +0100

    add styles

--- app/src/views/Projects.vue
+++ app/src/views/Projects.vue
@@ -1,16 +1,20 @@
 <template>
     <div id="projects">
-        <div class="project">
-            <div class="title">GitHub Pages Deployer</div>
-            <div class="url">https://github.com/marcterradas/githubpages-deployer</div>
-            <div class="tools"><b>Vue.js</b>,<b>Vite.js</b>,<b>JavaScript</b>,<b>SCSS</b>,<b>HTML</b></div>
-            <div class="description">{{ $t('projects.deployer.description') }}</div>
+        <div class="row">
+            <div class="project">
+                <div class="title">GitHub Pages Deployer</div>
+                <div class="url">https://github.com/marcterradas/githubpages-deployer</div>
+                <div class="tools"><b>Vue.js</b>,<b>Vite.js</b>,<b>JavaScript</b>,<b>SCSS</b>,<b>HTML</b></div>
+                <div class="description">{{ $t('projects.deployer.description') }}</div>
+            </div>
         </div>
-        <div class="project">
-            <div class="title">Portfolio</div>
-            <div class="url">https://github.com/marcterradas/portfolio</div>
-            <div class="tools"><b>Node.js</b>,<b>TypeScript</b>,<b>JEST</b></div>
-            <div class="description">{{ $t('projects.portfolio.description') }}</div>
+        <div class="row">
+            <div class="project">
+                <div class="title">Portfolio</div>
+                <div class="url">https://github.com/marcterradas/portfolio</div>
+                <div class="tools"><b>Node.js</b>,<b>TypeScript</b>,<b>JEST</b></div>
+                <div class="description">{{ $t('projects.portfolio.description') }}</div>
+            </div>
         </div>
     </div>
 </template>
@@ -23,9 +27,28 @@ export default {
 
 <style lang="scss">
 #projects {
-    .project {
-        b {
-            font-weight: bold;
+    padding: 40px;
+    height: fit-content;
+
+    .row {
+        width: 100%;
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+
+        .project {
+            border-radius: 4px;
+            padding: 20px;
+            box-shadow: 0px 4px 5px 1px rgba(0, 0, 0, 0.15);
+            background-color: $white;
+            margin-bottom: 20px;
+            width: 450px;
+            display: block;
+
+            b {
+                font-weight: bold;
+            }
         }
     }
 }
