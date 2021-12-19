ef8fd4e44d1a7f0544a8c747d85c6e343fb812a9
marcterradas <terradas.marc@gmail.com>
2021-12-19 08:12:27 +0100

    add runPHP project

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -2,9 +2,17 @@
     <div id="projects">
         <div class="row">
             <div class="project">
+                <div class="title">runPHP</div>
+                <div class="url"><a href="https://github.com/marcterradas/runPHP" target="_blank">github.com/marcterradas/runPHP</a></div>
+                <div class="tools"><b>Electron</b>,<b>TypeScript</b>,<b>JavaScript</b><b>Vue.js</b><b>Tailwind CSS</b><b>Vite.js</b></div>
+                <div class="description">{{ $t('projects.runPHP.description') }}</div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="project">
                 <div class="title">GitHub Pages Deployer</div>
                 <div class="url"><a href="https://github.com/marcterradas/githubpages-deployer" target="_blank">github.com/marcterradas/githubpages-deployer</a></div>
-                <div class="tools"><b>Vue.js</b>,<b>Vite.js</b>,<b>JavaScript</b>,<b>SCSS</b>,<b>HTML</b></div>
+                <div class="tools"><b>Node.js</b>,<b>TypeScript</b>,<b>JEST</b></div>
                 <div class="description">{{ $t('projects.deployer.description') }}</div>
             </div>
         </div>
@@ -12,7 +20,7 @@
             <div class="project">
                 <div class="title">Portfolio</div>
                 <div class="url"><a href="https://github.com/marcterradas/portfolio" target="_blank">github.com/marcterradas/portfolio</a></div>
-                <div class="tools"><b>Node.js</b>,<b>TypeScript</b>,<b>JEST</b></div>
+                <div class="tools"><b>Vue.js</b>,<b>JavaScript</b>,<b>SCSS</b>,<b>HTML</b>,<b>Vite.js</b></div>
                 <div class="description">{{ $t('projects.portfolio.description') }}</div>
             </div>
         </div>
@@ -67,7 +75,6 @@ export default {
             .tools {
                 margin-bottom: 10px;
             }
-
         }
     }
 }
