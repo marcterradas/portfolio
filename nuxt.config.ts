89c2fe4135b7392fd86c5c2c25c012d6f937f991
Marc Terradas <terradas.marc@gmail.com>
2022-08-25 18:30:55 +0200

    comment i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
 import { defineNuxtConfig } from "nuxt";
 
+// import en from "./assets/languages/en.json";
+// import es from "./assets/languages/es.json";
+// import cat from "./assets/languages/cat.json";
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    //   "@nuxtjs/i18n"
+  ],
   tailwindcss: {
     cssPath: "~/assets/css/tailwind.css",
     configPath: "tailwind.config.js",
@@ -11,4 +18,20 @@ export default defineNuxtConfig({
     injectPosition: 0,
     viewer: true,
   },
+  //   i18n: {
+  //     locales: [
+  //       { code: "en", iso: "en" },
+  //       { code: "es", iso: "es" },
+  //       { code: "cat", iso: "cat" },
+  //     ],
+  //     defaultLocale: "en",
+  //     vueI18n: {
+  //       fallbackLocale: "en",
+  //       messages: {
+  //         en: {},
+  //         es: {},
+  //         cat: {},
+  //       },
+  //     },
+  //   },
 });
