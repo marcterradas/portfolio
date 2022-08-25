cbd9a6022fa218e77a4e94e7eacef48864763d56
Marc Terradas <terradas.marc@gmail.com>
2022-08-25 17:33:48 +0200

    add i18n dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,14 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss'],
-    tailwindcss: {
-        cssPath: '~/assets/css/tailwind.css',
-        configPath: 'tailwind.config.js',
-        exposeConfig: false,
-        config: {},
-        injectPosition: 0,
-        viewer: true,
-      }
-})
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
+  tailwindcss: {
+    cssPath: "~/assets/css/tailwind.css",
+    configPath: "tailwind.config.js",
+    exposeConfig: false,
+    config: {},
+    injectPosition: 0,
+    viewer: true,
+  },
+});
