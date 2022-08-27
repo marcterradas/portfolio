5f033b7f456aa61313b6ded40997bde3a599335c
Marc Terradas <terradas.marc@gmail.com>
2022-08-27 17:59:22 +0200

    update app.js use translations

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import Navbar from "../components/Navbar";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <NextIntlProvider>
+    <NextIntlProvider messages={pageProps.messages}>
       <Navbar />
       <Component {...pageProps} />
     </NextIntlProvider>
