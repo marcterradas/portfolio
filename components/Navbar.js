f8510183646b44d3eaff7613210b5cdd3a06fc2d
Marc Terradas <terradas.marc@gmail.com>
2022-08-27 16:26:07 +0200

    init navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import LanguageSwitcher from "./LanguageSwitcher";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <LanguageSwitcher />
+    </nav>
+  );
+}
