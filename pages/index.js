2ecedaf04f1bc38fba139e3a491f58b9d4dd5b06
Marc Terradas <terradas.marc@gmail.com>
2022-08-27 16:28:47 +0200

    format home

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,3 @@
-
 export default function Home() {
-  return (
-    <div className="text-3xl font-bold underline">
-        Hello World
-    </div>
-  )
+  return <div className="text-3xl font-bold underline">Hello World</div>;
 }
