1e8ff014486e790eaa531643335c4990eed53441
Marc Terradas <terradas.marc@gmail.com>
2022-08-27 17:25:03 +0200

    init languages

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
+import { NextIntlProvider } from "next-intl";
+
 import "../styles/globals.css";
+
 import Navbar from "../components/Navbar";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <NextIntlProvider>
       <Navbar />
       <Component {...pageProps} />
-    </>
+    </NextIntlProvider>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
 export default function Home() {
   return <div></div>;
 }
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      messages: (await import(`../translations/${locale}.json`)).default,
+    },
+  };
+}
