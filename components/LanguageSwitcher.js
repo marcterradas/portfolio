1d1d7bb7587a74742656e3e2c6e9346eb0fcee02
Marc Terradas <terradas.marc@gmail.com>
2022-08-28 17:34:57 +0200

    init language dropdown

--- components/LanguageSwitcher.js
+++ components/LanguageSwitcher.js
@@ -4,13 +4,26 @@ import Link from "next/link";
 import Language from "./Language";
 
 export default function LanguageSwitcher() {
-  const { locales } = useRouter();
-  const langaugesComponents = locales.map((languageCode) => (
+  const { locale, locales } = useRouter();
+
+  const selectedLanguage = (
+    <div className="pr-2 flex justify-center items-center">
+      <Language languageCode={locale}></Language>
+      <div className="w-3 h-3 bg-[url('/img/arrow-down.svg')] bg-cover bg-no-repeat bg-center"></div>
+    </div>
+  );
+
+  const dropDown = locales.map((languageCode) => (
     <Link key={languageCode} href={languageCode} locale={languageCode}>
       <a>
         <Language languageCode={languageCode}></Language>
       </a>
     </Link>
   ));
-  return <div>{langaugesComponents}</div>;
+  return (
+    <>
+      <div>{selectedLanguage}</div>
+      <div>{dropDown}</div>
+    </>
+  );
 }
