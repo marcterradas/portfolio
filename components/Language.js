f2904f1f2090577bdc5513153545c9199f862461
Marc Terradas <terradas.marc@gmail.com>
2022-08-28 16:00:32 +0200

    update language styles

--- components/Language.js
+++ components/Language.js
@@ -6,18 +6,18 @@ export default function Language({ languageCode }) {
   const language = Languages[languageCode];
   const imgSrc = `/img/${languageCode}.svg`;
   return (
-    <div>
-      <div className="bg-black w-8 h-6 relative">
-        <Image
-          src={imgSrc}
-          alt={languageCode}
-          layout="fill"
-          objectFit="cover"
-        ></Image>
+    <Link href={languageCode} locale={languageCode}>
+      <div className="cursor-pointer flex items-center justify-start p-2">
+        <div className="w-8 h-6 relative">
+          <Image
+            src={imgSrc}
+            alt={languageCode}
+            layout="fill"
+            objectFit="cover"
+          ></Image>
+        </div>
+        <div className="ml-2 text-lg font-sans">{language}</div>
       </div>
-      <Link href={languageCode} locale={languageCode}>
-        {language}
-      </Link>
-    </div>
+    </Link>
   );
 }
