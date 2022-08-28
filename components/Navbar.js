18f82744f6f79bc912b1f0b4fc5983f00c2429d9
Marc Terradas <terradas.marc@gmail.com>
2022-08-28 18:23:57 +0200

    navbar, import selected language and language switcher

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { useRouter } from "next/router";
+
+import SelectedLanguage from "./SelectedLanguage";
 import LanguageSwitcher from "./LanguageSwitcher";
 
 export default function Navbar() {
+  const { locale: selectedLanguageCode, locales: languagesCodes } = useRouter();
+
   return (
     <nav className="flex justify-end pr-2">
-      <LanguageSwitcher />
+      <SelectedLanguage selectedLanguageCode={selectedLanguageCode} />
+      <LanguageSwitcher
+        selectedLanguageCode={selectedLanguageCode}
+        languagesCodes={languagesCodes}
+      />
     </nav>
   );
 }
