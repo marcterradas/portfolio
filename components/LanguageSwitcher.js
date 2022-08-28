7c87c63cd82c35147416480d186b4c7342a45dee
Marc Terradas <terradas.marc@gmail.com>
2022-08-28 18:06:36 +0200

    use component selected language

--- components/LanguageSwitcher.js
+++ components/LanguageSwitcher.js
@@ -2,28 +2,17 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 
 import Language from "./Language";
+import SelectedLanguage from "./SelectedLanguage";
 
 export default function LanguageSwitcher() {
-  const { locale, locales } = useRouter();
+  const { locale: selectedLanguageCode, locales: languagesCodes } = useRouter();
 
-  const selectedLanguage = (
-    <div className="pr-2 pl-1 flex justify-center items-center cursor-pointer hover:bg-stone-100 transition-colors ease-linear duration-500">
-      <Language languageCode={locale}></Language>
-      <div className="w-3 h-3 bg-[url('/img/arrow-down.svg')] bg-cover bg-no-repeat bg-center"></div>
-    </div>
-  );
-
-  const dropDown = locales.map((languageCode) => (
+  const dropDown = languagesCodes.map((languageCode) => (
     <Link key={languageCode} href={languageCode} locale={languageCode}>
       <a>
         <Language languageCode={languageCode}></Language>
       </a>
     </Link>
   ));
-  return (
-    <>
-      <div>{selectedLanguage}</div>
-      {/* <div>{dropDown}</div> */}
-    </>
-  );
+  return <SelectedLanguage selectedLanguageCode={selectedLanguageCode} />;
 }
