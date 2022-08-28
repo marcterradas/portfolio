81861b8ff74e9ef2969fa94deaa45eec7d146827
Marc Terradas <terradas.marc@gmail.com>
2022-08-28 17:34:41 +0200

    navbar add styles

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import LanguageSwitcher from "./LanguageSwitcher";
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav className="flex justify-end pr-2">
       <LanguageSwitcher />
     </nav>
   );
