654427d0288b329a016aac0a519a8338d6fbf786
Marc Terradas <terradas.marc@gmail.com>
2022-08-28 15:01:47 +0200

    use language component

--- components/LanguageSwitcher.js
+++ components/LanguageSwitcher.js
@@ -1,14 +1,10 @@
 import { useRouter } from "next/router";
-import Link from "next/link";
-
-import Languages from "../constants/languages.json";
+import Language from "./Language";
 
 export default function LanguageSwitcher() {
   const { locales } = useRouter();
   const langaugesComponents = locales.map((language) => (
-    <Link key={language} href={language} locale={language}>
-      {Languages[language]}
-    </Link>
+    <Language key={language} languageCode={language}></Language>
   ));
   return <div>{langaugesComponents}</div>;
 }
