9d21bd12e6950e2904be7edef2074aa6d384dda7
Marc Terradas <terradas.marc@gmail.com>
2022-08-28 15:32:52 +0200

    language image

--- components/Language.js
+++ components/Language.js
@@ -1,11 +1,23 @@
 import Link from "next/link";
+import Image from "next/image";
 import Languages from "../constants/languages.json";
 
 export default function Language({ languageCode }) {
   const language = Languages[languageCode];
+  const imgSrc = `/img/${languageCode}.svg`;
   return (
-    <Link href={languageCode} locale={languageCode}>
-      {language}
-    </Link>
+    <div>
+      <div className="bg-black w-8 h-6 relative">
+        <Image
+          src={imgSrc}
+          alt={languageCode}
+          layout="fill"
+          objectFit="cover"
+        ></Image>
+      </div>
+      <Link href={languageCode} locale={languageCode}>
+        {language}
+      </Link>
+    </div>
   );
 }
