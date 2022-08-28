3e0fa81b78a62ed5f6bd37e95d610dad003fec67
Marc Terradas <terradas.marc@gmail.com>
2022-08-28 16:19:01 +0200

    use link only in dropdown array

--- components/LanguageSwitcher.js
+++ components/LanguageSwitcher.js
@@ -1,10 +1,16 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
+
 import Language from "./Language";
 
 export default function LanguageSwitcher() {
   const { locales } = useRouter();
-  const langaugesComponents = locales.map((language) => (
-    <Language key={language} languageCode={language}></Language>
+  const langaugesComponents = locales.map((languageCode) => (
+    <Link key={languageCode} href={languageCode} locale={languageCode}>
+      <a>
+        <Language languageCode={languageCode}></Language>
+      </a>
+    </Link>
   ));
   return <div>{langaugesComponents}</div>;
 }
