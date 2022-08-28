f5f52b312b6fa6b41a7a91a41320eceb743bf2b6
Marc Terradas <terradas.marc@gmail.com>
2022-08-28 18:14:17 +0200

    don't show selected language in dropdown and sort languages

--- components/LanguageSwitcher.js
+++ components/LanguageSwitcher.js
@@ -7,12 +7,20 @@ import SelectedLanguage from "./SelectedLanguage";
 export default function LanguageSwitcher() {
   const { locale: selectedLanguageCode, locales: languagesCodes } = useRouter();
 
-  const dropDown = languagesCodes.map((languageCode) => (
-    <Link key={languageCode} href={languageCode} locale={languageCode}>
-      <a>
-        <Language languageCode={languageCode}></Language>
-      </a>
-    </Link>
-  ));
-  return <SelectedLanguage selectedLanguageCode={selectedLanguageCode} />;
+  const dropDown = languagesCodes
+    .filter((languageCode) => languageCode != selectedLanguageCode)
+    .sort()
+    .map((languageCode) => (
+      <Link key={languageCode} href={languageCode} locale={languageCode}>
+        <a className="pr-2 pl-1 cursor-pointer hover:bg-stone-100 transition-colors ease-linear duration-500">
+          <Language languageCode={languageCode}></Language>
+        </a>
+      </Link>
+    ));
+  return (
+    <>
+      <SelectedLanguage selectedLanguageCode={selectedLanguageCode} />
+      {dropDown}
+    </>
+  );
 }
