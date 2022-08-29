6016784427ee8c07e1f72f085fa3269210f6f2dc
Marc Terradas <terradas.marc@gmail.com>
2022-08-29 19:10:32 +0200

    language dropdown add callback function when click link to close popover

--- components/LanguageDropDown.js
+++ components/LanguageDropDown.js
@@ -4,13 +4,17 @@ import Language from "./Language";
 export default function LanguageDropDown({
   selectedLanguageCode,
   languagesCodes,
+  closePopover,
 }) {
   const dropDown = languagesCodes
     .filter((languageCode) => languageCode != selectedLanguageCode)
     .sort()
     .map((languageCode) => (
       <Link key={languageCode} href={languageCode} locale={languageCode}>
-        <a className="pr-2 pl-1 z-10 rounded-md cursor-pointer hover:bg-stone-100 transition-colors ease-linear duration-500">
+        <a
+          onClick={closePopover}
+          className="pr-2 pl-1 z-10 rounded-md cursor-pointer hover:bg-stone-100 transition-colors ease-linear duration-500"
+        >
           <Language languageCode={languageCode}></Language>
         </a>
       </Link>
