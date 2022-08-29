b6a750adf51e062073ce1700c87b006fa36aaf17
Marc Terradas <terradas.marc@gmail.com>
2022-08-29 17:21:04 +0200

    rename language switcher to language drop down

--- components/LanguageDropDown.js
+++ components/LanguageDropDown.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Language from "./Language";
 
-export default function LanguageSwitcher({
+export default function LanguageDropDown({
   selectedLanguageCode,
   languagesCodes,
 }) {
