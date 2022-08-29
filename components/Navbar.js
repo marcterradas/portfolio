76d780730318600c7b0a2955d50c5726eba9cf6b
Marc Terradas <terradas.marc@gmail.com>
2022-08-29 17:24:38 +0200

    init funcion open drop down

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,21 @@
 import { useRouter } from "next/router";
 import SelectedLanguage from "./SelectedLanguage";
-import LanguageSwitcher from "./LanguageSwitcher";
+import LanguageDropDown from "./LanguageDropDown";
 
 export default function Navbar() {
   const { locale: selectedLanguageCode, locales: languagesCodes } = useRouter();
 
+  function openDropDown() {
+    console.log("open drop down ...");
+  }
+
   return (
     <nav className="flex justify-end pr-2">
-      <SelectedLanguage selectedLanguageCode={selectedLanguageCode} />
-      <LanguageSwitcher
+      <SelectedLanguage
+        selectedLanguageCode={selectedLanguageCode}
+        openDropDown={openDropDown}
+      />
+      <LanguageDropDown
         selectedLanguageCode={selectedLanguageCode}
         languagesCodes={languagesCodes}
       />
