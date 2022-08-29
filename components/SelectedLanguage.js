a64d4f805cca3876b30369aadbf12a6e1576ce84
Marc Terradas <terradas.marc@gmail.com>
2022-08-29 18:21:35 +0200

    selected language remove callback function

--- components/SelectedLanguage.js
+++ components/SelectedLanguage.js
@@ -1,14 +1,8 @@
 import Language from "./Language";
 
-export default function SelectedLanguage({
-  selectedLanguageCode,
-  openDropDown,
-}) {
+export default function SelectedLanguage({ selectedLanguageCode }) {
   return (
-    <div
-      onClick={openDropDown}
-      className="pr-2 pl-1 flex justify-center items-center cursor-pointer hover:bg-stone-100 transition-colors ease-linear duration-500"
-    >
+    <div className="pr-2 pl-1 flex justify-center items-center cursor-pointer hover:bg-stone-100 transition-colors ease-linear duration-500">
       <Language languageCode={selectedLanguageCode}></Language>
       <div className="w-3 h-3 bg-[url('/img/arrow-down.svg')] bg-cover bg-no-repeat bg-center"></div>
     </div>
