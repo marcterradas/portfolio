a20ceff617285a5cdb0fc0e8abbf10aa9b1a9158
Marc Terradas <terradas.marc@gmail.com>
2022-08-30 09:26:35 +0200

    update styles language drop down element

--- components/language/LanguageDropDownElements.js
+++ components/language/LanguageDropDownElements.js
@@ -13,7 +13,7 @@ export default function LanguageDropDownElements({
       <Link key={languageCode} href={languageCode} locale={languageCode}>
         <a
           onClick={closePopover}
-          className="pr-2 pl-1 z-10 rounded-md cursor-pointer hover:bg-stone-100 transition-colors ease-linear duration-500"
+          className="pr-2 pl-1 z-10 rounded-md cursor-pointer hover:bg-stone-200 transition-colors ease-linear duration-300"
         >
           <Language languageCode={languageCode}></Language>
         </a>
