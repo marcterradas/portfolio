832372ec3a595d0e53366beff1034f95b9a906ec
Marc Terradas <terradas.marc@gmail.com>
2022-08-30 09:44:50 +0200

    update styles curriculum

--- components/buttons/Curriculum.js
+++ components/buttons/Curriculum.js
@@ -3,7 +3,7 @@ import { useTranslations } from "next-intl";
 export default function Curriculum() {
   const t = useTranslations("nav");
   return (
-    <button className="h-11 bg-blue-300 px-4 mr-4 rounded-sm text-lg font-sans font-bold hover:bg-blue-400 transition-colors ease-linear duration-300">
+    <button className="px-4 mr-4 font-sans text-base font-bold transition-colors duration-300 ease-linear bg-blue-300 rounded-sm h-11 hover:bg-blue-400 lg:text-lg">
       {t("curriculum")}
     </button>
   );
