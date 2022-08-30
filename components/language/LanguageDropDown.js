41c8cd026a165f18b36161191ab2ddb0298d8dfe
Marc Terradas <terradas.marc@gmail.com>
2022-08-30 09:48:30 +0200

    update duration time in popover transition

--- components/language/LanguageDropDown.js
+++ components/language/LanguageDropDown.js
@@ -14,7 +14,7 @@ export default function LanguageDropDown({
       </Popover.Button>
       <Transition
         as={Fragment}
-        enter="transition ease-out duration-200"
+        enter="transition ease-out duration-300"
         enterFrom="opacity-0 translate-y-1"
         enterTo="opacity-100 translate-y-0"
         leave="transition ease-in duration-150"
