f022f627980f546a3fc8b85e38db574ae6c00e76
Marc Terradas <terradas.marc@gmail.com>
2022-08-30 08:15:31 +0200

    remove comment code

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
-// import { useTranslations } from "next-intl";
 import Navbar from "../components/layout/Navbar";
 
 export default function Home() {
-  //   const t = useTranslations("home");
-  //   return <div>{t("language")}</div>;
   return (
     <>
       <Navbar />
