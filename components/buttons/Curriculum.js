ece960abb6efee4f04591ad2d580a8e87bfc31aa
Marc Terradas <terradas.marc@gmail.com>
2022-08-30 15:30:49 +0200

    update curriculum button styles

--- components/buttons/Curriculum.js
+++ components/buttons/Curriculum.js
@@ -1,10 +1,16 @@
+import Image from "next/image";
 import { useTranslations } from "next-intl";
 
 export default function Curriculum() {
   const t = useTranslations("nav");
   return (
-    <button className="px-4 mr-4 font-sans text-base font-bold transition-colors duration-300 ease-linear bg-blue-300 rounded-sm h-11 hover:bg-blue-400 sm:text-lg">
-      {t("curriculum")}
+    <button className="px-4 mr-4 transition-colors duration-300 ease-linear bg-blue-300 rounded-sm h-11 hover:bg-blue-400 sm:text-lg">
+      <div className="flex items-center justify-start cursor-pointer">
+        <div className="w-4 h-4 mr-3 bg-[url('/img/download.svg')] bg-cover bg-no-repeat bg-center"></div>
+        <div className="font-sans text-base font-bold sm:text-lg">
+          {t("curriculum")}
+        </div>
+      </div>
     </button>
   );
 }
