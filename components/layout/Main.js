38de59a068bd90fe86432bab1397272d88998693
Marc Terradas <terradas.marc@gmail.com>
2022-08-31 15:52:57 +0200

    import avatar component

--- components/layout/Main.js
+++ components/layout/Main.js
@@ -1,11 +1,13 @@
 import Title from "../description/Title";
 import SubTitle from "../description/SubTitle";
+import Avatar from "../description/Avatar";
 
 export default function Main() {
   return (
     <main className="flex flex-wrap justify-center pt-2 mt-16">
       <Title />
       <SubTitle />
+      <Avatar />
     </main>
   );
 }
