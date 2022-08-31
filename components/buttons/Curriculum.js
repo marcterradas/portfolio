98c9bfabca61a3a0c4012cbc86e6986e94193c23
Marc Terradas <terradas.marc@gmail.com>
2022-08-31 09:40:35 +0200

    add curriculum button

--- components/buttons/Curriculum.js
+++ components/buttons/Curriculum.js
@@ -1,16 +1,22 @@
-import Image from "next/image";
 import { useTranslations } from "next-intl";
+import Link from "next/link";
 
 export default function Curriculum() {
   const t = useTranslations("nav");
   return (
-    <button className="px-4 mr-4 transition-colors duration-300 ease-linear bg-blue-300 rounded-sm h-11 hover:bg-blue-400 sm:text-lg">
-      <div className="flex items-center justify-start cursor-pointer">
-        <div className="w-4 h-4 mr-3 bg-[url('/img/download.svg')] bg-cover bg-no-repeat bg-center"></div>
-        <div className="font-sans text-base font-bold sm:text-lg">
-          {t("curriculum")}
+    <Link href="/img/download.svg" locale={false} passHref>
+      <a
+        target="_blank"
+        donwload
+        className="flex justify-center px-4 mr-4 transition-colors duration-300 ease-linear bg-blue-300 rounded-sm h-11 hover:bg-blue-400 sm:text-lg"
+      >
+        <div className="flex items-center justify-start cursor-pointer">
+          <div className="w-4 h-4 mr-3 bg-[url('/img/download.svg')] bg-cover bg-no-repeat bg-center"></div>
+          <div className="font-sans text-base font-bold sm:text-lg">
+            {t("curriculum")}
+          </div>
         </div>
-      </div>
-    </button>
+      </a>
+    </Link>
   );
 }
