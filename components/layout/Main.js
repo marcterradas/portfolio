5d3b43f4eaaac10080e6f6858ecfba5f6d70c41d
Marc Terradas <terradas.marc@gmail.com>
2022-08-31 13:00:36 +0200

    main import subtitle

--- components/layout/Main.js
+++ components/layout/Main.js
@@ -1,9 +1,11 @@
 import Title from "../description/Title";
+import SubTitle from "../description/SubTitle";
 
 export default function Main() {
   return (
     <main className="flex flex-wrap justify-center pt-2 mt-16">
       <Title />
+      <SubTitle />
     </main>
   );
 }
