8c1e4909e46b3cd6ea1d559336ac49c6f73f1070
Marc Terradas <terradas.marc@gmail.com>
2022-08-31 12:40:35 +0200

    add title to curriculum button

--- components/buttons/Curriculum.js
+++ components/buttons/Curriculum.js
@@ -9,6 +9,7 @@ export default function Curriculum() {
         target="_blank"
         donwload
         className="flex justify-center px-4 mr-4 transition-colors duration-300 ease-linear bg-blue-300 rounded-sm h-11 hover:bg-blue-400 sm:text-lg"
+        title={t("curriculum")}
       >
         <div className="flex items-center justify-start cursor-pointer">
           <div className="w-4 h-4 mr-3 bg-[url('/img/download.svg')] bg-cover bg-no-repeat bg-center"></div>
