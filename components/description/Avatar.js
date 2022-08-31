b4729d3279db3ec5b23faf85cb75f1db729a35ac
Marc Terradas <terradas.marc@gmail.com>
2022-08-31 16:02:05 +0200

    avatar add image and styles

--- components/description/Avatar.js
+++ components/description/Avatar.js
@@ -1,3 +1,14 @@
+import Image from "next/image";
+
 export default function Avatar() {
-  return <div>Avatar ...</div>;
+  return (
+    <div className="relative flex items-center justify-center w-64 h-64 mt-6">
+      <Image
+        src="/img/avatar.svg"
+        alt="avatar"
+        layout="fill"
+        objectFit="cover"
+      ></Image>
+    </div>
+  );
 }
