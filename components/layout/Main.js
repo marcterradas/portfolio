b8a476558885ff367d5084d226160373a1d16621
Marc Terradas <terradas.marc@gmail.com>
2022-08-31 12:45:10 +0200

    update styles main

--- components/layout/Main.js
+++ components/layout/Main.js
@@ -2,7 +2,7 @@ import Title from "../description/Title";
 
 export default function Main() {
   return (
-    <main className="flex justify-center pt-2 mt-16">
+    <main className="flex flex-wrap justify-center pt-2 mt-16">
       <Title />
     </main>
   );
