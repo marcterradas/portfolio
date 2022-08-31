38d2d0bafcfe8f764a3d56c3ee1e886d809bd01f
Marc Terradas <terradas.marc@gmail.com>
2022-08-31 13:32:27 +0200

    subtitle update styles

--- components/description/SubTitle.js
+++ components/description/SubTitle.js
@@ -2,5 +2,9 @@ import { useTranslations } from "next-intl";
 
 export default function SubTitle() {
   const t = useTranslations("main");
-  return <h2>{t("subtitle")}</h2>;
+  return (
+    <h2 className="flex justify-center w-full font-sans text-lg font-normal">
+      {t("subtitle")}
+    </h2>
+  );
 }
