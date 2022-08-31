54348a7373fa2df5a3fd45036b010e4154a3a1cd
Marc Terradas <terradas.marc@gmail.com>
2022-08-31 11:45:40 +0200

    main import title component

--- components/layout/Main.js
+++ components/layout/Main.js
@@ -1,3 +1,9 @@
+import Title from "../description/Title";
+
 export default function Main() {
-  return <div className="mt-16">Main</div>;
+  return (
+    <main className="mt-16">
+      <Title />
+    </main>
+  );
 }
