bb7594afe1690db574811b068e541f1e4a2fe726
Marc Terradas <terradas.marc@gmail.com>
2022-09-01 17:04:36 +0200

    update styles avatar

--- components/description/Avatar.js
+++ components/description/Avatar.js
@@ -2,14 +2,16 @@ import Image from "next/image";
 
 export default function Avatar() {
   return (
-    <div className="relative flex items-center justify-center w-48 h-48 mt-6">
-      <Image
-        src="/img/avatar.svg"
-        alt="avatar"
-        layout="fill"
-        objectFit="cover"
-        priority={true}
-      ></Image>
+    <div className="flex items-center justify-center w-full">
+      <div className="relative flex items-center justify-center w-48 h-48 mt-6">
+        <Image
+          src="/img/avatar.svg"
+          alt="avatar"
+          layout="fill"
+          objectFit="cover"
+          priority={true}
+        ></Image>
+      </div>
     </div>
   );
 }
