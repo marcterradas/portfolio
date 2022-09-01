127b7677d4a88eb0a4a534d552bea754a7adfadc
Marc Terradas <terradas.marc@gmail.com>
2022-09-01 19:12:13 +0200

    update subtitles in large resolution

--- components/description/SubTitle.js
+++ components/description/SubTitle.js
@@ -3,7 +3,7 @@ import { useTranslations } from "next-intl";
 export default function SubTitle() {
   const t = useTranslations("main");
   return (
-    <h2 className="flex justify-center w-full font-sans text-lg font-normal">
+    <h2 className="flex justify-center w-full font-sans text-lg font-normal lg:text-2xl lg:mt-2">
       {t("subtitle")}
     </h2>
   );
