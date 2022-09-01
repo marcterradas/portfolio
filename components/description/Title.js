29c30bed80f4ee073056c2374507f282d7f1ff69
Marc Terradas <terradas.marc@gmail.com>
2022-09-01 19:12:53 +0200

    update title styles in large resolution

--- components/description/Title.js
+++ components/description/Title.js
@@ -1,6 +1,6 @@
 export default function Title() {
   return (
-    <h1 className="flex justify-center w-full font-sans text-3xl font-bold">
+    <h1 className="flex justify-center w-full font-sans text-3xl font-bold lg:text-5xl">
       Marc Terradas Zapata
     </h1>
   );
