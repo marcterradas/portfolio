103c87579aa0cf3a84e2bec92b3d674f4316069f
Marc Terradas <terradas.marc@gmail.com>
2022-09-02 06:59:05 +0200

    update language selected styles

--- components/language/LanguageSelected.js
+++ components/language/LanguageSelected.js
@@ -2,7 +2,7 @@ import Language from "./Language";
 
 export default function SelectedLanguage({ selectedLanguageCode }) {
   return (
-    <div className="flex items-center justify-center pl-1 pr-2 transition-colors duration-300 ease-linear rounded-md cursor-pointer h-11 hover:bg-stone-200">
+    <div className="flex items-center justify-center pl-1 pr-2 transition-colors duration-300 ease-linear rounded-sm cursor-pointer h-11 hover:bg-stone-200">
       <Language languageCode={selectedLanguageCode}></Language>
       <div className="w-3 h-3 bg-[url('/img/arrow-down.svg')] bg-cover bg-no-repeat bg-center"></div>
     </div>
