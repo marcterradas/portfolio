16a18ba06a19c10210b3372afd22f7ad6f3aa219
Marc Terradas <terradas.marc@gmail.com>
2022-09-02 16:28:05 +0200

    about me, update styles

--- components/description/AboutMe.js
+++ components/description/AboutMe.js
@@ -4,7 +4,7 @@ export default function AboutMe() {
   const t = useTranslations("main");
 
   return (
-    <div>
+    <div className="pl-4 pr-2 mt-8 font-sans text-base font-normal sm:mt-12 sm:text-lg sm:px-0">
       <p>{t("aboutMe.part1")}</p>
       <p>{t("aboutMe.part2", { experienceYears: 4 })}</p>
     </div>
