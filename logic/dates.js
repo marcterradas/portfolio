24c0ef5e04a95b039f3e80377d15b6cb06a2e7b9
Marc Terradas <terradas.marc@gmail.com>
2022-09-02 18:21:54 +0200

    calculateDifferenceyears, validate params

--- logic/dates.js
+++ logic/dates.js
@@ -1,3 +1,8 @@
-export function calculateDifferenceYears() {
-  return 0;
+export function calculateDifferenceYears(year, month) {
+  if (typeof year !== "number") return false;
+  if (typeof month !== "month") return false;
+
+  let difference = 0;
+
+  return difference;
 }
