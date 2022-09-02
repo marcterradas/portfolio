f944c59ce8a35c2359740e958f9a441cb31e04d8
Marc Terradas <terradas.marc@gmail.com>
2022-09-02 20:11:11 +0200

    add new test

--- test/dates.js
+++ test/dates.js
@@ -10,5 +10,6 @@ test("calculateDifferenceYears, validate params", () => {
 test("calculateDifferenceYears, validate output", () => {
   expect(calculateDifferenceYears("2022-01", "2022-01")).toBe(0);
   expect(calculateDifferenceYears("2018-06", "2022-06")).toBe(4);
+  expect(calculateDifferenceYears("2018-06", "2022-08")).toBe(4);
   expect(calculateDifferenceYears("2018-06", "2023-06")).toBe(5);
 });
