8e53e8370d7d19e92663f45ea1e1e8b52eaed36b
Marc Terradas <terradas.marc@gmail.com>
2022-09-02 19:53:36 +0200

    calculate difference years, add tests

--- test/dates.js
+++ test/dates.js
@@ -4,5 +4,11 @@ import { calculateDifferenceYears } from "../logic/dates";
 test("calculateDifferenceYears, validate params", () => {
   expect(calculateDifferenceYears()).toBe(false);
   expect(calculateDifferenceYears("")).toBe(false);
-  expect(calculateDifferenceYears("", "")).toBe(0);
+  expect(calculateDifferenceYears("", "")).toBe(false);
+});
+
+test("calculateDifferenceYears, validate output", () => {
+  expect(calculateDifferenceYears("2022-01", "2022-01")).toBe(0);
+  expect(calculateDifferenceYears("2018-06", "2022-06")).toBe(4);
+  expect(calculateDifferenceYears("2018-06", "2023-06")).toBe(5);
 });
