7721fe389a0e4051da544e003559e19af56724ae
Marc Terradas <terradas.marc@gmail.com>
2022-09-02 19:20:55 +0200

    add jsdoc

--- logic/dates.js
+++ logic/dates.js
@@ -1,3 +1,9 @@
+/**
+ *
+ * @param {string} date1
+ * @param {string} date2
+ * @returns {number} difference between two dates
+ */
 export function calculateDifferenceYears(date1, date2) {
   if (typeof date1 !== "string") return false;
   if (typeof date2 !== "string") return false;
