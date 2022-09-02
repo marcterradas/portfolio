51a3b17add51f546649a685c1999064325452682
Marc Terradas <terradas.marc@gmail.com>
2022-09-02 20:11:27 +0200

    calculate experience years

--- components/description/AboutMe.js
+++ components/description/AboutMe.js
@@ -1,12 +1,19 @@
 import { useTranslations } from "next-intl";
+import { calculateDifferenceYears } from "../../logic/dates";
+import Dates from "../../constants/dates.json";
 
 export default function AboutMe() {
   const t = useTranslations("main");
+  const [currentDate] = new Date().toISOString().split("T");
+  const experienceYears = calculateDifferenceYears(
+    Dates.firstJobDate,
+    currentDate
+  );
 
   return (
     <div className="pl-4 pr-2 mt-8 font-sans text-base font-normal sm:mt-12 sm:text-lg sm:px-0">
       <p>{t("aboutMe.part1")}</p>
-      <p>{t("aboutMe.part2", { experienceYears: 4 })}</p>
+      <p>{t("aboutMe.part2", { experienceYears: experienceYears })}</p>
     </div>
   );
 }
