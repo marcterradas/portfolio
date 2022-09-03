e8b4d72a306d8af86b3fcfb37b817cf93cb474cf
Marc Terradas <terradas.marc@gmail.com>
2022-09-03 06:55:36 +0200

    init calculate difference years and months method and update documentation previous function

--- logic/dates.js
+++ logic/dates.js
@@ -1,25 +1,21 @@
 /**
- *
+ * Calculate difference in years between two dates
  * @param {string} startDate valid date
  * @param {string} currentDate valid date
  * @returns {number} difference between two dates
  */
 export function calculateDifferenceYears(startDate, currentDate) {
-  // validate params if are dates
   if (isNaN(Date.parse(startDate))) return false;
   if (isNaN(Date.parse(startDate))) return false;
 
-  // get year and month from params
   let [startDateYear, startDateMonth] = startDate.split("-");
   let [currentDateYear, currentDateMonth] = currentDate.split("-");
 
-  // transform strings to integers
   startDateYear = parseInt(startDateYear);
   startDateMonth = parseInt(startDateMonth);
   currentDateYear = parseInt(currentDateYear);
   currentDateMonth = parseInt(currentDateMonth);
 
-  // calculate year difference
   let difference = currentDateYear - startDateYear;
 
   /*
@@ -28,6 +24,26 @@ export function calculateDifferenceYears(startDate, currentDate) {
   */
   if (currentDateYear > startDateYear && currentDateMonth < startDateMonth)
     difference--;
+
+  return difference;
+}
+
+/**
+ * Calculate difference in years and months between two dates
+ * @param {string} startDate valid date
+ * @param {string} currentDate valid date
+ * @returns {Array<number>} two positions, first difference in years
+ * and the second difference in months
+ */
+export default function calculateDifferenceYearsAndMonths(
+  startDate,
+  currentDate
+) {
+  const yearDifference = calculateDifferenceYears(startDate, currentDate);
+
+  if (!yearDifference) return false;
+
+  let difference = [yearDifference, 0];
 
   return difference;
 }
