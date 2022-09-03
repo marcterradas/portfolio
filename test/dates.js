b2fb66912621d1e30c2c57f45de8bee8fc08f9a1
Marc Terradas <terradas.marc@gmail.com>
2022-09-03 07:04:36 +0200

    add calculateDifferenceYearsAndMonths test for params

--- test/dates.js
+++ test/dates.js
@@ -1,5 +1,8 @@
 import { expect, test } from "vitest";
-import { calculateDifferenceYears } from "../logic/dates";
+import {
+  calculateDifferenceYears,
+  calculateDifferenceYearsAndMonths,
+} from "../logic/dates";
 
 test("calculateDifferenceYears, validate params", () => {
   expect(calculateDifferenceYears()).toBe(false);
@@ -12,4 +15,10 @@ test("calculateDifferenceYears, validate output", () => {
   expect(calculateDifferenceYears("2018-06", "2022-06")).toBe(4);
   expect(calculateDifferenceYears("2018-06", "2022-08")).toBe(4);
   expect(calculateDifferenceYears("2018-06", "2023-06")).toBe(5);
+});
+
+test("calculateDifferenceYearsAndMonths, validate params", () => {
+  expect(calculateDifferenceYearsAndMonths()).toBe(false);
+  expect(calculateDifferenceYearsAndMonths("")).toBe(false);
+  expect(calculateDifferenceYearsAndMonths("", "")).toBe(false);
 });
