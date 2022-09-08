fdd25ccbdad8e93df0a7b7f5d5177dd7588a8949
Marc Terradas <terradas.marc@gmail.com>
2022-09-08 21:39:25 +0200

    update phoenix script

--- phoenix.sh
+++ phoenix.sh
@@ -1,7 +1,7 @@
 # remove .next node_modules folders and package-lock.json
-rm -rf .next
-rm -rf node_modules
-rm -rf package-lock.json
+rm -rf ./.next
+rm -rf ./node_modules
+rm -rf ./package-lock.json
 
 # install dependencies
 npm i
