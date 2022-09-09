12c2462e2023f2cb96e14b9bc7cd5e6ff1e90caf
Marc Terradas <terradas.marc@gmail.com>
2022-09-09 15:13:54 +0200

    import kriter component

--- layout/Experience.js
+++ layout/Experience.js
@@ -1,11 +1,13 @@
 import Title from "../components/experience/Title";
 import Andy from "../components/experience/Andy";
+import Kriter from "../components/experience/Kriter";
 
 export default function Experience() {
   return (
     <section>
       <Title />
       <Andy />
+      <Kriter />
     </section>
   );
 }
