228005a71990b31efc9eab1c94332b0f4f4ad8de
Marc Terradas <terradas.marc@gmail.com>
2022-09-09 15:13:02 +0200

    import andy component

--- layout/Experience.js
+++ layout/Experience.js
@@ -1,9 +1,11 @@
 import Title from "../components/experience/Title";
+import Andy from "../components/experience/Andy";
 
 export default function Experience() {
   return (
     <section>
       <Title />
+      <Andy />
     </section>
   );
 }
