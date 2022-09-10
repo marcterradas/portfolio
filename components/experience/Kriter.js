3ef8c1a41ab54aeac4a97118fbfd5c519d0057c0
Marc Terradas <terradas.marc@gmail.com>
2022-09-10 10:08:12 +0200

    update kriter styles

--- components/experience/Kriter.js
+++ components/experience/Kriter.js
@@ -1,7 +1,9 @@
 export default function Kriter() {
   return (
     <div>
-      <h4>Kriter</h4>
+      <h4 className="font-sans text-lg font-normal lg:text-xl lg:mt-2">
+        Kriter
+      </h4>
     </div>
   );
 }
