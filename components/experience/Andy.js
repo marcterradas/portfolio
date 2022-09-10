cf711cee8c7bab6125fdc042a69f120ce070f20f
Marc Terradas <terradas.marc@gmail.com>
2022-09-10 13:15:49 +0200

    andy init duration

--- components/experience/Andy.js
+++ components/experience/Andy.js
@@ -1,13 +1,21 @@
 import { useCustomTranslations } from "../../logic/translations";
+import Dates from "../../constants/dates.json";
 
 export default function Andy() {
   const translations = useCustomTranslations("experience");
+  const [currentDate] = new Date().toISOString().split("T");
+  let { startDate, endDate } = Dates.andy;
+
+  if (!endDate) endDate = currentDate;
 
   return (
     <div>
       <h4 className="font-sans text-lg font-normal lg:text-xl lg:mt-2">
         {translations("andy.title")}
       </h4>
+      <p className="font-sans text-base font-normal sm:text-lg">
+        {translations("andy.duration")}
+      </p>
     </div>
   );
 }
