35eb0cf3897e361cdd843e5cb9e400a3ada0b218
Marc Terradas <terradas.marc@gmail.com>
2022-09-10 11:43:50 +0200

    update andy translations

--- components/experience/Andy.js
+++ components/experience/Andy.js
@@ -1,7 +1,13 @@
+import { useCustomTranslations } from "../../logic/translations";
+
 export default function Andy() {
+  const translations = useCustomTranslations("experience");
+
   return (
     <div>
-      <h4 className="font-sans text-lg font-normal lg:text-xl lg:mt-2">Andy</h4>
+      <h4 className="font-sans text-lg font-normal lg:text-xl lg:mt-2">
+        {translations("andy.title")}
+      </h4>
     </div>
   );
 }
