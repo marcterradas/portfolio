47f25f4b19649e1cc2316f34de571d9f7af89fb4
Marc Terradas <terradas.marc@gmail.com>
2022-09-10 12:27:59 +0200

    experience, update title styles

--- components/experience/Title.js
+++ components/experience/Title.js
@@ -3,7 +3,7 @@ import { useCustomTranslations } from "../../logic/translations";
 export default function Title() {
   const translations = useCustomTranslations("experience");
   return (
-    <h3 className="font-sans text-xl font-bold lg:text-2xl">
+    <h3 className="mb-2 font-sans text-xl font-bold lg:text-2xl lg:mb-4">
       {translations("title")}
     </h3>
   );
