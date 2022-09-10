aba72e9811461f05a08488392fc9a66496edd800
Marc Terradas <terradas.marc@gmail.com>
2022-09-10 11:43:59 +0200

    update kriter, use translations

--- components/experience/Kriter.js
+++ components/experience/Kriter.js
@@ -1,8 +1,12 @@
+import { useCustomTranslations } from "../../logic/translations";
+
 export default function Kriter() {
+  const translations = useCustomTranslations("experience");
+
   return (
     <div>
       <h4 className="font-sans text-lg font-normal lg:text-xl lg:mt-2">
-        Kriter
+        {translations("kriter.title")}
       </h4>
     </div>
   );
