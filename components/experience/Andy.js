18839b607455b493d754c969e1f6bc1f427dca65
Marc Terradas <terradas.marc@gmail.com>
2022-09-10 10:08:05 +0200

    update andy styles

--- components/experience/Andy.js
+++ components/experience/Andy.js
@@ -1,9 +1,7 @@
 export default function Andy() {
   return (
     <div>
-      <h4 className="font-sans text-lg font-normal lg:text-2xl lg:mt-2">
-        Andy
-      </h4>
+      <h4 className="font-sans text-lg font-normal lg:text-xl lg:mt-2">Andy</h4>
     </div>
   );
 }
