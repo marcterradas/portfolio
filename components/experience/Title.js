1fbbe9ebf9c9f4b2d50a4d933cdf4ad838cf96df
Marc Terradas <terradas.marc@gmail.com>
2022-09-10 09:58:40 +0200

    update experience title

--- components/experience/Title.js
+++ components/experience/Title.js
@@ -2,5 +2,5 @@ import { useTranslations } from "next-intl";
 
 export default function Title() {
   const t = useTranslations("experience");
-  return <h3>{t("title")}</h3>;
+  return <h3 className="text-xl font-bold lg:text-2xl">{t("title")}</h3>;
 }
