9edb35c0b87a839f6b7ecd1ec9c6c012535c3aae
Marc Terradas <terradas.marc@gmail.com>
2022-09-11 12:12:25 +0200

    andy add translation

--- components/experience/Andy.js
+++ components/experience/Andy.js
@@ -1,4 +1,4 @@
-import { Disclosure } from "@headlessui/react";
+import { Disclosure, Transition } from "@headlessui/react";
 import { useCustomTranslations } from "../../logic/translations";
 import { calculateDifferenceYearsAndMonths } from "../../logic/dates";
 import Dates from "../../constants/dates.json";
@@ -36,20 +36,29 @@ export default function Andy() {
               } w-3 h-3 bg-[url('/img/arrow-down.svg')] bg-cover bg-no-repeat bg-center`}
             ></div>
           </Disclosure.Button>
-          <Disclosure.Panel className="bg-blue-200">
-            <div>
-              <h5>{translations("responsibilities")}</h5>
-              <p>{translations("andy.responsibilities.part1")}</p>
-              <p>{translations("andy.responsibilities.part2")}</p>
-              <p>{translations("andy.responsibilities.part3")}</p>
-            </div>
-            <div>
-              <h5>{translations("objectivesAchieved")}</h5>
-              <p>{translations("andy.objectivesAchieved.part1")}</p>
-              <p>{translations("andy.objectivesAchieved.part2")}</p>
-              <p>{translations("andy.objectivesAchieved.part3")}</p>
-            </div>
-          </Disclosure.Panel>
+          <Transition
+            enter="transition ease-out duration-300"
+            enterFrom="opacity-0 translate-y-1"
+            enterTo="opacity-100 translate-y-0"
+            leave="transition ease-in duration-150"
+            leaveFrom="opacity-100 translate-y-0"
+            leaveTo="opacity-0 translate-y-1"
+          >
+            <Disclosure.Panel className="bg-blue-200">
+              <div>
+                <h5>{translations("responsibilities")}</h5>
+                <p>{translations("andy.responsibilities.part1")}</p>
+                <p>{translations("andy.responsibilities.part2")}</p>
+                <p>{translations("andy.responsibilities.part3")}</p>
+              </div>
+              <div>
+                <h5>{translations("objectivesAchieved")}</h5>
+                <p>{translations("andy.objectivesAchieved.part1")}</p>
+                <p>{translations("andy.objectivesAchieved.part2")}</p>
+                <p>{translations("andy.objectivesAchieved.part3")}</p>
+              </div>
+            </Disclosure.Panel>
+          </Transition>
         </>
       )}
     </Disclosure>
