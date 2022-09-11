ee33b59fb47fdde11cf243e0cc25e9bf89930d6b
Marc Terradas <terradas.marc@gmail.com>
2022-09-11 21:02:34 +0200

    create title skill

---
+++ components/skills/Title.js
@@ -0,0 +1,10 @@
+import { useCustomTranslations } from "../../logic/translations";
+
+export default function Title() {
+  const translations = useCustomTranslations("skills");
+  return (
+    <h3 className="mb-2 font-sans text-xl font-bold lg:text-2xl lg:mb-4">
+      {translations("title")}
+    </h3>
+  );
+}
