38f4d254c0131cbeca42540b7106b8b0d87bef61
Marc Terradas <terradas.marc@gmail.com>
2022-09-11 16:18:35 +0200

    andy update styles

--- components/experience/Andy.js
+++ components/experience/Andy.js
@@ -13,10 +13,10 @@ export default function Andy() {
   const [years, months] = calculateDifferenceYearsAndMonths(startDate, endDate);
 
   return (
-    <Disclosure as="div" defaultOpen="true" className="mb-8">
+    <Disclosure as="div" defaultOpen="true" className="mb-4">
       {({ open }) => (
         <>
-          <Disclosure.Button className="flex items-center justify-center w-full px-4 text-left ">
+          <Disclosure.Button className="flex items-center justify-center w-full px-4 py-2 text-left transition-colors duration-300 ease-linear rounded-sm bg-stone-200 hover:bg-stone-300 ">
             <div className="w-full">
               <h4 className="font-sans text-lg font-normal lg:text-xl">
                 {translations("andy.title")}
@@ -46,18 +46,20 @@ export default function Andy() {
             leaveFrom="opacity-100 translate-y-0"
             leaveTo="opacity-0 translate-y-1"
           >
-            <Disclosure.Panel className="bg-blue-200">
-              <div>
-                <h5>{translations("responsibilities")}</h5>
-                <p>{translations("andy.responsibilities.part1")}</p>
-                <p>{translations("andy.responsibilities.part2")}</p>
-                <p>{translations("andy.responsibilities.part3")}</p>
-              </div>
-              <div>
-                <h5>{translations("objectivesAchieved")}</h5>
-                <p>{translations("andy.objectivesAchieved.part1")}</p>
-                <p>{translations("andy.objectivesAchieved.part2")}</p>
-                <p>{translations("andy.objectivesAchieved.part3")}</p>
+            <Disclosure.Panel className="px-4 py-3">
+              <div className="px-4 py-2 rounded-sm bg-stone-200">
+                <div className="mb-4">
+                  <h5>{translations("responsibilities")}</h5>
+                  <p>{translations("andy.responsibilities.part1")}</p>
+                  <p>{translations("andy.responsibilities.part2")}</p>
+                  <p>{translations("andy.responsibilities.part3")}</p>
+                </div>
+                <div>
+                  <h5>{translations("objectivesAchieved")}</h5>
+                  <p>{translations("andy.objectivesAchieved.part1")}</p>
+                  <p>{translations("andy.objectivesAchieved.part2")}</p>
+                  <p>{translations("andy.objectivesAchieved.part3")}</p>
+                </div>
               </div>
             </Disclosure.Panel>
           </Transition>
