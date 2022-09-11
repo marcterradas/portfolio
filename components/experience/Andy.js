4e31fc6e76c534e24870716612577b1192d1efc2
Marc Terradas <terradas.marc@gmail.com>
2022-09-11 21:21:27 +0200

    andy use disclosure

--- components/experience/Andy.js
+++ components/experience/Andy.js
@@ -1,7 +1,7 @@
-import { Disclosure, Transition } from "@headlessui/react";
 import { useCustomTranslations } from "../../logic/translations";
 import { calculateDifferenceYearsAndMonths } from "../../logic/dates";
 import Dates from "../../constants/dates.json";
+import CustomDisclosure from "../common/Disclosure";
 
 export default function Andy() {
   const translations = useCustomTranslations("experience");
@@ -12,83 +12,68 @@ export default function Andy() {
 
   const [years, months] = calculateDifferenceYearsAndMonths(startDate, endDate);
 
-  return (
-    <Disclosure as="div" defaultOpen="true" className="mb-8">
-      {({ open }) => (
-        <>
-          <Disclosure.Button className="flex items-center justify-center w-full px-4 py-2 text-left transition-colors duration-300 ease-linear rounded-sm bg-stone-100 hover:bg-stone-200 ">
-            <div className="w-full">
-              <h4 className="font-sans text-lg font-normal lg:text-xl">
-                {translations("andy.title")}
-              </h4>
-              <p className="font-sans text-sm font-normal sm:text-base">
-                {translations.rich("andy.duration", {
-                  years,
-                  months,
-                  bold: (children) => <b>{children}</b>,
-                })}
-              </p>
-              <p className="font-sans text-sm font-normal sm:text-base">
-                {translations("andy.ubication")}
-              </p>
-            </div>
-            <div
-              className={`${
-                open ? "rotate-180 transform" : ""
-              } w-3 h-3 bg-[url('/img/arrow-down.svg')] bg-cover bg-no-repeat bg-center`}
-            ></div>
-          </Disclosure.Button>
-          <Transition
-            enter="transition ease-out duration-300"
-            enterFrom="opacity-0 translate-y-1"
-            enterTo="opacity-100 translate-y-0"
-            leave="transition ease-in duration-150"
-            leaveFrom="opacity-100 translate-y-0"
-            leaveTo="opacity-0 translate-y-1"
-          >
-            <Disclosure.Panel className="px-2 py-3">
-              <div className="px-4 py-2 rounded-sm bg-stone-100">
-                <div className="mb-6">
-                  <h5 className="mb-1 font-sans text-base font-normal underline sm:text-lg">
-                    {translations("responsibilities")}
-                  </h5>
-                  <div className="pl-4">
-                    <ul className="font-sans text-sm font-normal list-disc sm:text-base">
-                      <li className="mb-1">
-                        {translations("andy.responsibilities.part1")}
-                      </li>
-                      <li className="mb-1">
-                        {translations("andy.responsibilities.part2")}
-                      </li>
-                      <li className="mb-1">
-                        {translations("andy.responsibilities.part3")}
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <div>
-                  <h5 className="mb-1 font-sans text-base font-normal underline sm:text-lg">
-                    {translations("objectivesAchieved")}
-                  </h5>
-                  <div className="pl-4">
-                    <ul className="font-sans text-sm font-normal list-disc sm:text-base">
-                      <li className="mb-1">
-                        {translations("andy.objectivesAchieved.part1")}
-                      </li>
-                      <li className="mb-1">
-                        {translations("andy.objectivesAchieved.part2")}
-                      </li>
-                      <li className="mb-1">
-                        {translations("andy.objectivesAchieved.part3")}
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </Disclosure.Panel>
-          </Transition>
-        </>
-      )}
-    </Disclosure>
-  );
+  const Button = () => {
+    return (
+      <div className="w-full">
+        <h4 className="font-sans text-lg font-normal lg:text-xl">
+          {translations("andy.title")}
+        </h4>
+        <p className="font-sans text-sm font-normal sm:text-base">
+          {translations.rich("andy.duration", {
+            years,
+            months,
+            bold: (children) => <b>{children}</b>,
+          })}
+        </p>
+        <p className="font-sans text-sm font-normal sm:text-base">
+          {translations("andy.ubication")}
+        </p>
+      </div>
+    );
+  };
+
+  const Content = () => {
+    return (
+      <>
+        <div className="mb-6">
+          <h5 className="mb-1 font-sans text-base font-normal underline sm:text-lg">
+            {translations("responsibilities")}
+          </h5>
+          <div className="pl-4">
+            <ul className="font-sans text-sm font-normal list-disc sm:text-base">
+              <li className="mb-1">
+                {translations("andy.responsibilities.part1")}
+              </li>
+              <li className="mb-1">
+                {translations("andy.responsibilities.part2")}
+              </li>
+              <li className="mb-1">
+                {translations("andy.responsibilities.part3")}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <h5 className="mb-1 font-sans text-base font-normal underline sm:text-lg">
+            {translations("objectivesAchieved")}
+          </h5>
+          <div className="pl-4">
+            <ul className="font-sans text-sm font-normal list-disc sm:text-base">
+              <li className="mb-1">
+                {translations("andy.objectivesAchieved.part1")}
+              </li>
+              <li className="mb-1">
+                {translations("andy.objectivesAchieved.part2")}
+              </li>
+              <li className="mb-1">
+                {translations("andy.objectivesAchieved.part3")}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  return <CustomDisclosure button={<Button />} content={<Content />} />;
 }
