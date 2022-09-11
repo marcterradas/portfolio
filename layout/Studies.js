1f8f8e2058f6d7d6b24dac7369b7f00daee385b9
Marc Terradas <terradas.marc@gmail.com>
2022-09-11 20:51:03 +0200

    studies, use title component

--- layout/Studies.js
+++ layout/Studies.js
@@ -1,3 +1,9 @@
+import Title from "../components/studies/Title";
+
 export default function Studies() {
-  return <section className="h-screen">Studies...</section>;
+  return (
+    <section className="h-auto min-h-screen">
+      <Title />
+    </section>
+  );
 }
