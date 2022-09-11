bdb1301ce6343200a564214638d9fe0c0e8fe854
Marc Terradas <terradas.marc@gmail.com>
2022-09-11 16:38:16 +0200

    update kriter styles

--- components/experience/Kriter.js
+++ components/experience/Kriter.js
@@ -16,20 +16,22 @@ export default function Kriter() {
     <Disclosure defaultOpen="true" className="mb-8">
       {({ open }) => (
         <>
-          <Disclosure.Button className="w-full bg-red-200">
-            <h4 className="font-sans text-lg font-normal lg:text-xl lg:mt-2">
-              {translations("kriter.title")}
-            </h4>
-            <p className="font-sans text-base font-normal sm:text-lg">
-              {translations.rich("kriter.duration", {
-                years,
-                months,
-                bold: (children) => <b>{children}</b>,
-              })}
-            </p>
-            <p className="font-sans text-base font-normal sm:text-lg">
-              {translations("kriter.ubication")}
-            </p>
+          <Disclosure.Button className="flex items-center justify-center w-full px-4 py-2 text-left transition-colors duration-300 ease-linear rounded-sm bg-stone-100 hover:bg-stone-200 ">
+            <div className="w-full">
+              <h4 className="font-sans text-lg font-normal lg:text-xl">
+                {translations("kriter.title")}
+              </h4>
+              <p className="font-sans text-base font-normal sm:text-lg">
+                {translations.rich("kriter.duration", {
+                  years,
+                  months,
+                  bold: (children) => <b>{children}</b>,
+                })}
+              </p>
+              <p className="font-sans text-sm font-normal sm:text-base">
+                {translations("kriter.ubication")}
+              </p>
+            </div>
             <div
               className={`${
                 open ? "rotate-180 transform" : ""
@@ -44,16 +46,38 @@ export default function Kriter() {
             leaveFrom="opacity-100 translate-y-0"
             leaveTo="opacity-0 translate-y-1"
           >
-            <Disclosure.Panel className="bg-blue-200">
-              <div>
-                <h5>{translations("responsibilities")}</h5>
-                <p>{translations("kriter.responsibilities.part1")}</p>
-                <p>{translations("kriter.responsibilities.part2")}</p>
-              </div>
-              <div>
-                <h5>{translations("objectivesAchieved")}</h5>
-                <p>{translations("kriter.objectivesAchieved.part1")}</p>
-                <p>{translations("kriter.objectivesAchieved.part2")}</p>
+            <Disclosure.Panel className="px-2 py-3">
+              <div className="px-4 py-2 rounded-sm bg-stone-100">
+                <div className="mb-6">
+                  <h5 className="mb-1 font-sans text-base font-normal underline sm:text-lg">
+                    {translations("responsibilities")}
+                  </h5>
+                  <div className="pl-4">
+                    <ul className="font-sans text-sm font-normal list-disc sm:text-base">
+                      <li className="mb-1">
+                        {translations("kriter.responsibilities.part1")}
+                      </li>
+                      <li className="mb-1">
+                        {translations("kriter.responsibilities.part2")}
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div>
+                  <h5 className="mb-1 font-sans text-base font-normal underline sm:text-lg">
+                    {translations("objectivesAchieved")}
+                  </h5>
+                  <div className="pl-4">
+                    <ul className="font-sans text-sm font-normal list-disc sm:text-base">
+                      <li className="mb-1">
+                        {translations("kriter.objectivesAchieved.part1")}
+                      </li>
+                      <li className="mb-1">
+                        {translations("kriter.objectivesAchieved.part2")}
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </div>
             </Disclosure.Panel>
           </Transition>
