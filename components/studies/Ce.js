54a8a1e76b1c799bd29b851fd464e48f08ba7983
Marc Terradas <terradas.marc@gmail.com>
2022-09-12 19:46:17 +0200

    update ce component

--- components/studies/Ce.js
+++ components/studies/Ce.js
@@ -1,3 +1,36 @@
+import { useCustomTranslations } from "../../logic/translations";
+import CustomDisclosure from "../common/Disclosure";
+
 export default function Ce() {
-  return <div>Ce ...</div>;
+  const translations = useCustomTranslations("studies");
+
+  const Button = () => {
+    return (
+      <div className="w-full">
+        <h4 className="font-sans text-lg font-normal lg:text-xl">
+          {translations("ce.title")}
+        </h4>
+        <p className="font-sans text-sm font-normal sm:text-base">
+          {translations("ce.place")}
+        </p>
+        <p className="font-sans text-sm font-normal sm:text-base">
+          {translations("ce.duration")}
+        </p>
+      </div>
+    );
+  };
+
+  const Content = () => {
+    return (
+      <div className="mb-6">
+        <div className="pl-4">
+          <ul className="font-sans text-sm font-normal list-disc sm:text-base">
+            <li className="">{translations("ce.description")}</li>
+          </ul>
+        </div>
+      </div>
+    );
+  };
+
+  return <CustomDisclosure button={<Button />} content={<Content />} />;
 }
