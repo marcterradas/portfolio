7f34e8783a9ba60d276d183e57d7014a4ae12bd8
Marc Terradas <terradas.marc@gmail.com>
2022-09-12 19:29:37 +0200

    fix bug

--- layout/Studies.js
+++ layout/Studies.js
@@ -1,7 +1,7 @@
 import Title from "../components/studies/Title";
 import Ce from "../components/studies/Ce";
 import Daw from "../components/studies/Daw";
-import B2 from "../components/studies/b2";
+import B2 from "../components/studies/B2";
 
 export default function Studies() {
   return (
