98ddeaf619bb67bdb024e22cbc4dc306134dd01e
Marc Terradas <terradas.marc@gmail.com>
2022-09-12 19:17:19 +0200

    import b2 component

--- layout/Studies.js
+++ layout/Studies.js
@@ -1,6 +1,7 @@
 import Title from "../components/studies/Title";
 import Ce from "../components/studies/Ce";
 import Daw from "../components/studies/Daw";
+import B2 from "../components/studies/b2";
 
 export default function Studies() {
   return (
@@ -8,6 +9,7 @@ export default function Studies() {
       <Title />
       <Ce />
       <Daw />
+      <B2 />
     </section>
   );
 }
