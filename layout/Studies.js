3e63ead64edce00fa2d63828bf1ca596c0f28b05
Marc Terradas <terradas.marc@gmail.com>
2022-09-12 19:16:39 +0200

    import Ce component

--- layout/Studies.js
+++ layout/Studies.js
@@ -1,9 +1,11 @@
 import Title from "../components/studies/Title";
+import Ce from "../components/studies/Ce";
 
 export default function Studies() {
   return (
     <section className="h-auto min-h-screen">
       <Title />
+      <Ce />
     </section>
   );
 }
