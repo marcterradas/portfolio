a8bb64f660af8f90770102df61e2f9862cc5a507
Marc Terradas <terradas.marc@gmail.com>
2022-09-12 19:16:58 +0200

    import daw component

--- layout/Studies.js
+++ layout/Studies.js
@@ -1,11 +1,13 @@
 import Title from "../components/studies/Title";
 import Ce from "../components/studies/Ce";
+import Daw from "../components/studies/Daw";
 
 export default function Studies() {
   return (
     <section className="h-auto min-h-screen">
       <Title />
       <Ce />
+      <Daw />
     </section>
   );
 }
