31f505ee5bcd1a608be1fafcf6a4dd90c6c896f6
Marc Terradas <terradas.marc@gmail.com>
2022-09-13 19:20:27 +0200

    init skills

--- layout/Skills.js
+++ layout/Skills.js
@@ -1,6 +1,8 @@
 import Title from "../components/skills/Title";
+import SkillsList from "../constants/skills";
 
 export default function Skills() {
+  const SkillsElements = SkillsList.map((skill) => console.log(skill));
   return (
     <section className="h-[calc(100vh-4rem)]">
       <Title />
