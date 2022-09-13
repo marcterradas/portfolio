dbc2f9e90d6b10e080a75972c1ca489fdde52e2a
Marc Terradas <terradas.marc@gmail.com>
2022-09-13 19:53:40 +0200

    init show skills

--- layout/Skills.js
+++ layout/Skills.js
@@ -1,11 +1,15 @@
 import Title from "../components/skills/Title";
 import SkillsList from "../constants/skills";
+import Skill from "../components/skills/Skill";
 
 export default function Skills() {
-  const SkillsElements = SkillsList.map((skill) => console.log(skill));
+  const SkillsElements = SkillsList.map((skillName) => (
+    <Skill key={skillName} skillName={skillName} />
+  ));
   return (
     <section className="h-[calc(100vh-4rem)]">
       <Title />
+      <div>{SkillsElements}</div>
     </section>
   );
 }
