9303a78d411d73faa3e647426b369c4787b30092
Marc Terradas <terradas.marc@gmail.com>
2022-09-13 20:54:31 +0200

    fix skill component styles

--- components/skills/Skill.js
+++ components/skills/Skill.js
@@ -1,5 +1,7 @@
 export default function Skill({ skillName }) {
   return (
-    <p className="font-sans text-base font-normal sm:text-lg">{skillName}</p>
+    <div className="font-sans text-base font-normal sm:text-lg">
+      {skillName}
+    </div>
   );
 }
