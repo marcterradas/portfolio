bd0b3a5835a88923cb39f2db1ec6c6f966ea0cbe
Marc Terradas <terradas.marc@gmail.com>
2022-09-14 21:19:51 +0200

    linkedin add link

--- components/footer/Linkedin.js
+++ components/footer/Linkedin.js
@@ -1,9 +1,11 @@
 import { useCustomTranslations } from "../../logic/translations";
 import ContactButton from "../common/ContactButton";
+import contactLinks from "../../constants/contactLinks.json";
 
 export default function Linkedin() {
   const translations = useCustomTranslations("footer");
   const name = translations("linkedin");
+  const link = contactLinks.linkedin;
   return (
     <div>
       <ContactButton />
