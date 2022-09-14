a90fe6d16c6c06253dab0e6254adb518123ba9ef
Marc Terradas <terradas.marc@gmail.com>
2022-09-14 21:12:03 +0200

    github, get name from translations

--- components/footer/Github.js
+++ components/footer/Github.js
@@ -1,6 +1,9 @@
+import { useCustomTranslations } from "../../logic/translations";
 import ContactButton from "../common/ContactButton";
 
 export default function Github() {
+  const translations = useCustomTranslations("footer");
+  const name = translations("github");
   return (
     <div>
       <ContactButton />
