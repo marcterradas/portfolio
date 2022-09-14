adaa5efcddfd91dfc046c33e19f3599baaa5f91a
Marc Terradas <terradas.marc@gmail.com>
2022-09-14 21:12:38 +0200

    email, get name from translations

--- components/footer/Email.js
+++ components/footer/Email.js
@@ -1,6 +1,9 @@
+import { useCustomTranslations } from "../../logic/translations";
 import ContactButton from "../common/ContactButton";
 
 export default function Email() {
+  const translations = useCustomTranslations("footer");
+  const name = translations("email");
   return (
     <div>
       <ContactButton />
