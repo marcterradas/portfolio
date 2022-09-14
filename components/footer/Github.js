1f3f89adfa9e1162063b78c2b400cb33d13d026d
Marc Terradas <terradas.marc@gmail.com>
2022-09-14 21:28:35 +0200

    github, add image

--- components/footer/Github.js
+++ components/footer/Github.js
@@ -6,6 +6,8 @@ export default function Github() {
   const translations = useCustomTranslations("footer");
   const name = translations("github");
   const link = contactLinks.github;
+  const image = "/img/github.svg";
+
   return (
     <div>
       <ContactButton />
