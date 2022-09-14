a5325563d021ce34bd430b10bffcdf2d077ac5f6
Marc Terradas <terradas.marc@gmail.com>
2022-09-14 21:19:36 +0200

    email, add link

--- components/footer/Email.js
+++ components/footer/Email.js
@@ -1,9 +1,11 @@
 import { useCustomTranslations } from "../../logic/translations";
 import ContactButton from "../common/ContactButton";
+import contactLinks from "../../constants/contactLinks.json";
 
 export default function Email() {
   const translations = useCustomTranslations("footer");
   const name = translations("email");
+  const link = contactLinks.email;
   return (
     <div>
       <ContactButton />
