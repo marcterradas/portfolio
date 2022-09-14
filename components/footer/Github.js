8932b702a10205678a00c52c5957e86fd6a317f1
Marc Terradas <terradas.marc@gmail.com>
2022-09-14 21:19:42 +0200

    github add linnk

--- components/footer/Github.js
+++ components/footer/Github.js
@@ -1,9 +1,11 @@
 import { useCustomTranslations } from "../../logic/translations";
 import ContactButton from "../common/ContactButton";
+import contactLinks from "../../constants/contactLinks.json";
 
 export default function Github() {
   const translations = useCustomTranslations("footer");
   const name = translations("github");
+  const link = contactLinks.github;
   return (
     <div>
       <ContactButton />
