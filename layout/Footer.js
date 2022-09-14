52cfe785ae0d5d0eef2a1cf56717662bcb543240
Marc Terradas <terradas.marc@gmail.com>
2022-09-14 20:04:43 +0200

    import email component

--- layout/Footer.js
+++ layout/Footer.js
@@ -1,11 +1,13 @@
 import Linkedin from "../components/footer/Linkedin";
 import Github from "../components/footer/Github";
+import Email from "../components/footer/Email";
 
 export default function Footer() {
   return (
     <footer className="h-16">
       <Linkedin />
       <Github />
+      <Email />
     </footer>
   );
 }
