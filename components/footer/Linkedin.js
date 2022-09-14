d39e73cfb40c9d9fbe310d18da26ee12c9612a50
Marc Terradas <terradas.marc@gmail.com>
2022-09-14 21:12:29 +0200

    linkedin, get name from translations

--- components/footer/Linkedin.js
+++ components/footer/Linkedin.js
@@ -1,6 +1,9 @@
+import { useCustomTranslations } from "../../logic/translations";
 import ContactButton from "../common/ContactButton";
 
 export default function Linkedin() {
+  const translations = useCustomTranslations("footer");
+  const name = translations("linkedin");
   return (
     <div>
       <ContactButton />
