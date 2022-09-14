478730f263b3f20ac204b9724f18c2471af47df8
Marc Terradas <terradas.marc@gmail.com>
2022-09-14 20:03:32 +0200

    import linkedin

--- layout/Footer.js
+++ layout/Footer.js
@@ -1,3 +1,9 @@
+import Linkedin from "../components/footer/Linkedin";
+
 export default function Footer() {
-  return <footer className="h-16">Footer ...</footer>;
+  return (
+    <footer className="h-16">
+      <Linkedin></Linkedin>
+    </footer>
+  );
 }
