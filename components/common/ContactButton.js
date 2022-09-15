66ab90f19fb4cf0ea9c645c026298aca2eeade7d
Marc Terradas <terradas.marc@gmail.com>
2022-09-15 21:14:44 +0200

    Contact button, add custom colors

--- components/common/ContactButton.js
+++ components/common/ContactButton.js
@@ -2,10 +2,14 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function ContactButton({ name, link, image, color }) {
+  const borderColor = `border-${color}`;
+  const bgHover = `hover:bg-${color}`;
   return (
     <Link href={link} locale={false} passHref>
       <a target="_blank" className="" title={name}>
-        <div className="flex items-center justify-start p-2 border-2">
+        <div
+          className={`flex items-center justify-start p-2 border-2 ${borderColor} ${bgHover}`}
+        >
           <div className="relative w-8 h-8 mr-2">
             <Image src={image} alt={name} layout="fill" objectFit="cover" />
           </div>
