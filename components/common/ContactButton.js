bd74170c822db8fe501695c843c37d382b9166c7
Marc Terradas <terradas.marc@gmail.com>
2022-09-15 20:42:43 +0200

    contact button, update image styles

--- components/common/ContactButton.js
+++ components/common/ContactButton.js
@@ -1,11 +1,14 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function ContactButton({ name, link, image, color }) {
   return (
     <Link href={link} locale={false} passHref>
       <a target="_blank" className="" title={name}>
-        <div>
-          <div></div>
+        <div className="flex items-center justify-start p-2 border-2">
+          <div className="relative w-8 h-8 mr-2">
+            <Image src={image} alt={name} layout="fill" objectFit="cover" />
+          </div>
           <div className="font-sans text-base font-normal sm:text-lg">
             {name}
           </div>
