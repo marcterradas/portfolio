6474e97f8e33a1651395358940224e579144428f
Marc Terradas <terradas.marc@gmail.com>
2022-09-15 21:42:27 +0200

    footer, update gap

--- layout/Footer.js
+++ layout/Footer.js
@@ -4,7 +4,7 @@ import Email from "../components/footer/Email";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col items-center justify-center gap-4 mb-4 md:flex-row">
+    <footer className="flex flex-col items-center justify-center gap-2 mb-4 md:flex-row md:gap-4">
       <Linkedin />
       <Github />
       <Email />
