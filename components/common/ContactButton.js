3edca8e023ff6acf7e11c25d22cf4ef871da4072
Marc Terradas <terradas.marc@gmail.com>
2022-09-15 19:41:40 +0200

    init contact button

--- components/common/ContactButton.js
+++ components/common/ContactButton.js
@@ -1,4 +1,16 @@
+import Link from "next/link";
+
 export default function ContactButton({ name, link, image, color }) {
-  console.log(name, link, image, color);
-  return <div>{name}</div>;
+  return (
+    <Link href={link} locale={false} passHref>
+      <a target="_blank" className="" title={name}>
+        <div>
+          <div></div>
+          <div className="font-sans text-base font-normal sm:text-lg">
+            {name}
+          </div>
+        </div>
+      </a>
+    </Link>
+  );
 }
