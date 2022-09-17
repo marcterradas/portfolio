2adb612bd0409b40254954b68b33f2dff1acd44b
Marc Terradas <terradas.marc@gmail.com>
2022-09-17 11:02:50 +0200

    rename header to head

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import { NextIntlProvider } from "next-intl";
 import "../styles/globals.css";
-import Header from "../components/head/Head";
+import Head from "../components/head/Head";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <NextIntlProvider messages={pageProps.messages}>
-      <Header />
+      <Head />
       <Component {...pageProps} />
     </NextIntlProvider>
   );
