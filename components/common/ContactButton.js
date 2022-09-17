48794e937e6e328376848ab3e2e955e8408d34a4
Marc Terradas <terradas.marc@gmail.com>
2022-09-17 21:47:00 +0200

    remove empty class attribute

--- components/common/ContactButton.js
+++ components/common/ContactButton.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function ContactButton({ name, link, image }) {
   return (
     <Link href={link} locale={false} passHref>
-      <a target="_blank" className="" title={name}>
+      <a target="_blank" title={name}>
         <div className="flex items-center justify-start w-40 h-12 p-2 transition-colors duration-300 ease-linear border-2 rounded-sm border-stone-200 hover:bg-stone-200">
           <div className="relative w-8 h-8 mr-2">
             <Image src={image} alt={name} layout="fill" objectFit="cover" />
