2ca208b17accba82dd7f6fcc2ae2fcbcab6af6c5
Marc Terradas <terradas.marc@gmail.com>
2022-09-17 11:02:28 +0200

    import custom header

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import { NextIntlProvider } from "next-intl";
 import "../styles/globals.css";
+import Header from "../components/head/Head";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <NextIntlProvider messages={pageProps.messages}>
+      <Header />
       <Component {...pageProps} />
     </NextIntlProvider>
   );
