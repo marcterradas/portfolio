e96d0f34f788a643fd5f8cc2a8dc7f41b788e407
Marc Terradas <terradas.marc@gmail.com>
2023-03-18 15:45:17 +0100

    projects import title component

--- layout/Projects.js
+++ layout/Projects.js
@@ -1,3 +1,9 @@
+import Title from "../components/projects/Title";
+
 export default function Projects() {
-  return <section></section>;
+  return (
+    <section className="h-auto min-h-screen">
+      <Title />
+    </section>
+  );
 }
