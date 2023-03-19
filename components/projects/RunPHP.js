e0ade045b5ba267bca6a468a8bfd5598656a2766
Marc Terradas <terradas.marc@gmail.com>
2023-03-19 16:01:26 +0100

    runPHP add info

--- components/projects/RunPHP.js
+++ components/projects/RunPHP.js
@@ -1,3 +1,18 @@
+import ProjectsInformation from "../../constants/projects.json";
+import SectionContainer from "../common/SectionContainer";
+import Subtitle from "../common/Subtitle";
+import Paragraph from "../common/Paragraph";
+
 export default function RunPHP() {
-  return <div>RunPHP</div>;
+  const { runPHP } = ProjectsInformation;
+  const { title, link, technologies } = runPHP;
+  const skillsStr = technologies.toString();
+  return (
+    <SectionContainer>
+      <div> image ...</div>
+      <Subtitle>{title}</Subtitle>
+      <Paragraph>Description ...</Paragraph>
+      <Paragraph>{skillsStr}</Paragraph>
+    </SectionContainer>
+  );
 }
