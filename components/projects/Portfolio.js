a60288bb5a9c1d2cdf36dabde05bf7768502faac
Marc Terradas <terradas.marc@gmail.com>
2023-03-19 11:28:18 +0100

    init portfolio component

--- components/projects/Portfolio.js
+++ components/projects/Portfolio.js
@@ -1,3 +1,18 @@
+import ProjectsInformation from "../../constants/projects.json";
+import SectionContainer from "../common/SectionContainer";
+import Subtitle from "../common/Subtitle";
+import Paragraph from "../common/Paragraph";
+
 export default function Portfolio() {
-  return <div>Portfolio</div>;
+  const { portfolio } = ProjectsInformation;
+  const { title, link, technologies } = portfolio;
+  const skillsStr = technologies.toString();
+  return (
+    <SectionContainer>
+      <div> image ...</div>
+      <Subtitle>{title}</Subtitle>
+      <Paragraph>Description ...</Paragraph>
+      <Paragraph>{skillsStr}</Paragraph>
+    </SectionContainer>
+  );
 }
