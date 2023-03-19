4b2567b20dbb3d3b147dee230ea537f49e10b3ae
Marc Terradas <terradas.marc@gmail.com>
2023-03-19 17:55:32 +0100

    update height images

--- components/common/ProjectImage.js
+++ components/common/ProjectImage.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function ProjectImage({ image }) {
   return (
     <div className="flex items-center justify-center w-full">
-      <div className="relative flex items-center justify-center w-40 h-40 mt-6 lg:w-48 lg:h-48">
+      <div className="relative flex items-center justify-center w-48 h-48 mt-6 lg:w-56 lg:h-56">
         <Image
           src={`/img/${image}.png`}
           alt={image}
