db4a568bb06b5dbb2c233fb5b8fff462428b8778
Marc Terradas <terradas.marc@gmail.com>
2023-05-06 12:17:41 +0200

    language use drop down from infrastructure

--- components/navbar/LanguageDropDown.js
+++ components/navbar/LanguageDropDown.js
@@ -1,5 +1,8 @@
-import { Popover, Transition } from "@headlessui/react";
-import { Fragment } from "react";
+import {
+  DropDownWrapper,
+  DropDownButton,
+  DropDownContent,
+} from "@/infrastructure/DropDown";
 import LanguageSelected from "@/components/navbar/LanguageSelected";
 import LanguageDropDownElements from "@/components/navbar/LanguageDropDownElements";
 
@@ -8,31 +11,21 @@ export default function LanguageDropDown({
   languagesCodes,
 }) {
   return (
-    <Popover className="relative w-36">
-      <Popover.Button>
+    <DropDownWrapper>
+      <DropDownButton>
         <LanguageSelected selectedLanguageCode={selectedLanguageCode} />
-      </Popover.Button>
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-300"
-        enterFrom="opacity-0 translate-y-1"
-        enterTo="opacity-100 translate-y-0"
-        leave="transition ease-in duration-150"
-        leaveFrom="opacity-100 translate-y-0"
-        leaveTo="opacity-0 translate-y-1"
-      >
-        <Popover.Panel className="absolute z-10">
-          {({ close: closePopover }) => (
-            <div className="grid gap-2 rounded-sm shadow-md">
-              <LanguageDropDownElements
-                selectedLanguageCode={selectedLanguageCode}
-                languagesCodes={languagesCodes}
-                closePopover={closePopover}
-              />
-            </div>
-          )}
-        </Popover.Panel>
-      </Transition>
-    </Popover>
+      </DropDownButton>
+      <DropDownContent>
+        {({ close: closePopover }) => (
+          <div className="grid gap-2 rounded-sm shadow-md">
+            <LanguageDropDownElements
+              selectedLanguageCode={selectedLanguageCode}
+              languagesCodes={languagesCodes}
+              closePopover={closePopover}
+            />
+          </div>
+        )}
+      </DropDownContent>
+    </DropDownWrapper>
   );
 }
