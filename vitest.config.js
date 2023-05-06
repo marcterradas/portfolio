b2712d74e9fe8c61f93bbb3a3cd57d1821accc02
Marc Terradas <terradas.marc@gmail.com>
2023-05-06 13:40:56 +0200

    update vitest config

--- vitest.config.js
+++ vitest.config.js
@@ -1,8 +1,14 @@
+import path from "path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     globals: true,
     include: ["**/test/*.{js,tsx,ts}"],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./"),
+    },
   },
 });
