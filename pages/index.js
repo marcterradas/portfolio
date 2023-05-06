b10e392b5cfbc9c3db8e6ebc04f5617fd8623a2b
Marc Terradas <terradas.marc@gmail.com>
2023-05-06 11:48:15 +0200

    move layout components to controller folder

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Head from "@/controller/Head";
 import Navbar from "@/controller/Navbar";
-import Main from "@/components/layout/Main";
-import Experience from "@/components/layout/Experience";
-import Studies from "@/components/layout/Studies";
-import Projects from "@/components/layout/Projects";
-import Skills from "@/components/layout/Skills";
-import Footer from "@/components/layout/Footer";
+import Main from "@/controller/Main";
+import Experience from "@/controller/Experience";
+import Studies from "@/controller/Studies";
+import Projects from "@/controller/Projects";
+import Skills from "@/controller/Skills";
+import Footer from "@/controller/Footer";
 
 export default function Home() {
   return (
