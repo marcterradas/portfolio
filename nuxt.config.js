2630372fbacf574a00a1f79f5fb87ee14d0f6f99
Marc Terradas <terradas.marc@gmail.com>
2024-05-01 19:27:55 +0200

    lint code

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,13 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
-    modules: [
-        '@nuxt/eslint'
-    ],
-    eslint: {
-        config: {
-          stylistic: true 
-        }
+  modules: [
+    '@nuxt/eslint',
+  ],
+  eslint: {
+    config: {
+      stylistic: true,
     },
-    css: ['~/src/styles/reset.css']
+  },
+  css: ['~/src/styles/reset.css'],
 })
