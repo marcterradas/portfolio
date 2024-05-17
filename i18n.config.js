587eccebf4e1c2883765cdafd19ec89946b5e239
Marc Terradas <terradas.marc@gmail.com>
2024-05-17 16:03:21 +0200

    Update i18n.config.js

--- i18n.config.js
+++ i18n.config.js
@@ -1,7 +1,8 @@
+import { defineI18nConfig } from '#imports'
+
 import cat from '@/infrastructure/translations/cat.json'
 import es from '@/infrastructure/translations/es.json'
 import en from '@/infrastructure/translations/en.json'
-import { defineI18nConfig } from '#imports'
 
 export default defineI18nConfig(() => ({
   legacy: false,
