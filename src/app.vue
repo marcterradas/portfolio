f4720a068f42d17e9745b031d2306c005dea30e8
Marc Terradas <terradas.marc@gmail.com>
2024-07-14 11:01:56 +0200

    Add description container in app

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,6 @@
 <script setup>
 import NavigationContainer from '@/application/NavigationContainer.vue'
+import DescriptionContainer from '@/application/DescriptionContainer.vue'
 </script>
 
 <template>
@@ -8,7 +9,7 @@ import NavigationContainer from '@/application/NavigationContainer.vue'
       <NavigationContainer />
     </nav>
     <main>
-      <div>{{ $t('test') }}</div>
+      <DescriptionContainer />
     </main>
   </div>
 </template>
@@ -19,6 +20,9 @@ import NavigationContainer from '@/application/NavigationContainer.vue'
     margin: 0 auto;
     padding: calc(var(--spacer)*1.5);
     max-width: var(--max-width);
+    display: flex;
+    flex-direction: column;
+    gap: calc(var(--spacer)*4);
 
     @media screen and (min-width: 1024px) {
       padding: calc(var(--spacer)*2);
