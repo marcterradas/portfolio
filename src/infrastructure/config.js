33f7aa76eaadb9715df9de3939445785acd6674e
Marc Terradas <terradas.marc@gmail.com>
2024-07-14 11:29:26 +0200

    Create config.js

---
+++ src/infrastructure/config.js
@@ -0,0 +1,23 @@
+export default {
+  contactLinks: {
+    github: 'https://github.com/marcterradas',
+    linkedin: 'https://www.linkedin.com/in/marc-terradas-zapata',
+    codewars: 'https://www.codewars.com/users/marcterradas',
+    email: 'terradas.marc@gmail.com',
+  },
+  dates: {
+    dogfy: {
+      startDate: '2023-12',
+      endDate: null,
+    },
+    andy: {
+      startDate: '2019-06',
+      endDate: '2023-12',
+    },
+    kriter: {
+      startDate: '2018-06',
+      endDate: '2019-06',
+    },
+  },
+  skills: ['Vue.js', 'Nuxt.js', 'Node.js', 'JavaScript', 'PHP', 'Java', 'MySQL', 'MongoDB', 'HTML', 'CSS', 'SCSS', 'GIT'],
+}
