195cc72b887f9b980b1529887d1636e34d4264b7
Marc Terradas <terradas.marc@gmail.com>
2024-07-27 19:50:30 +0200

    Update BaseChip.vue

--- src/application/BaseChip.vue
+++ src/application/BaseChip.vue
@@ -4,13 +4,21 @@ const props = defineProps({
     type: String,
     default: null,
   },
+  link: {
+    type: String,
+    required: true,
+  },
 })
 
 const hasIcon = computed(() => !!props.icon)
 </script>
 
 <template>
-  <div class="base-link">
+  <a
+    :href="props.link"
+    target="_blank"
+    class="base-link"
+  >
     <img
       v-if="hasIcon"
       class="base-link__icon"
@@ -18,23 +26,25 @@ const hasIcon = computed(() => !!props.icon)
       alt="icon"
     >
     <slot />
-  </div>
+  </a>
 </template>
 
 <style>
     .base-link {
-        display: flex;
-        align-items: center;
-        background-color: var(--black);
-        border: var(--default-border) solid var(--dark-gray);
-        border-radius: var(--default-border-radius);
-        color: var(--dark-gray);
-        cursor: pointer;
-        font-size: var(--font-sm);
-        gap: calc(var(--spacer)/4);
-        padding: calc(var(--spacer)/3);
-        transition: var(--transition-background);
-        width: fit-content;
+      align-items: center;
+      background-color: var(--black);
+      border: var(--default-border) solid var(--dark-gray);
+      border-radius: var(--default-border-radius);
+      color: var(--white);
+      cursor: pointer;
+      display: flex;
+      font-size: var(--font-sm);
+      font-weight: bold;
+      gap: calc(var(--spacer)/4);
+      padding: calc(var(--spacer)/3);
+      text-decoration: none;
+      transition: var(--transition-background);
+      width: fit-content;
 
         @media screen and (min-width: 1024px) {
             font-size: var(--font-md);
