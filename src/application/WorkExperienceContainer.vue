07a80bbcc41747161247e16865d515409c7a9aad
Marc Terradas <terradas.marc@gmail.com>
2024-07-28 16:43:03 +0200

    Init work experience container.

--- src/application/WorkExperienceContainer.vue
+++ src/application/WorkExperienceContainer.vue
@@ -2,9 +2,28 @@
 </script>
 
 <template>
-  <div>TODO: Work Experience ...</div>
+  <div class="work-experience-container">
+    <h3 class="work-experience-container__title">
+      {{ $t('workExperienceContainer.label.title') }}
+    </h3>
+    <div>TODO: Dogfy</div>
+    <div>TODO: Andy</div>
+    <div>TODO: Kriter</div>
+  </div>
 </template>
 
 <style>
+.work-experience-container {
+  display: flex;
+  flex-direction: column;
+  gap:var(--spacer);
+}
 
+.work-experience-container__title {
+  font-size: var(--font-lg);
+
+  @media screen and (min-width: 1024px) {
+    font-size: var(--font-xl);
+  }
+}
 </style>
