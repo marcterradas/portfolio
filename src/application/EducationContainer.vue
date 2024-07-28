8873736364f287dc663041b594620c8ccb3c7f01
Marc Terradas <terradas.marc@gmail.com>
2024-07-28 16:43:19 +0200

    Init education container.

--- src/application/EducationContainer.vue
+++ src/application/EducationContainer.vue
@@ -2,9 +2,27 @@
 </script>
 
 <template>
-  <div>TODO: Education ...</div>
+  <div class="education-container">
+    <h3 class="education-container__title">
+      {{ $t('educationContainer.label.title') }}
+    </h3>
+    <div>TODO: DAW</div>
+    <div>TODO: C1</div>
+  </div>
 </template>
 
 <style>
+.education-container {
+  display: flex;
+  flex-direction: column;
+  gap:var(--spacer);
+}
 
+.education-container__title {
+  font-size: var(--font-lg);
+
+  @media screen and (min-width: 1024px) {
+    font-size: var(--font-xl);
+  }
+}
 </style>
