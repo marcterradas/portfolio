14e0a24c155057ad6b6d7c85950151c83b33f9f9
Marc Terradas <terradas.marc@gmail.com>
2024-07-28 14:34:38 +0200

    move sortui to dev dependencies

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,20 +3,25 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   srcDir: 'src/',
   ssr: true,
+
   modules: [
     '@nuxt/eslint',
     '@nuxt/test-utils/module',
     '@nuxtjs/i18n',
   ],
+
   i18n: {
     locales: ['en', 'es', 'cat'],
     defaultLocale: 'en',
     vueI18n: './i18n.config.js',
   },
+
   eslint: {
     config: {
       stylistic: true,
     },
   },
+
   css: ['@/styles/normalize.min.css', '@/styles/tokens.css', '@/styles/main.css', '@/styles/fonts.css'],
+  compatibilityDate: '2024-07-28',
 })
