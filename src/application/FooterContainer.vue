d05afedb4bf902442ef70430b20cd6aa34acf297
Marc Terradas <terradas.marc@gmail.com>
2024-07-28 15:35:08 +0200

    Update FooterContainer.vue

--- src/application/FooterContainer.vue
+++ src/application/FooterContainer.vue
@@ -1,9 +1,28 @@
 <script setup>
+import config from '@/infrastructure/config.js'
 </script>
 
 <template>
   <div class="footer-container">
-    TODO: Footer...
+    <a
+      :href="config.contactLinks.linkedin"
+      class="footer-container__link"
+      target="_blank"
+    >
+      {{ $t('common.label.linkedin') }}
+    </a>
+    <a
+      :href="config.contactLinks.github"
+      class="footer-container__link"
+      target="_blank"
+    >{{ $t('common.label.github') }}
+    </a>
+    <a
+      :href="config.contactLinks.email"
+      class="footer-container__link"
+      target="_blank"
+    >{{ $t('common.label.email') }}
+    </a>
   </div>
 </template>
 
@@ -11,5 +30,21 @@
 .footer-container {
   display: flex;
   justify-content: center;
+  gap: var(--spacer);
+}
+
+.footer-container__link {
+  font-size: var(--font-sm);
+  color: var(--dark-gray);
+  cursor: pointer;
+  transition: var(--transition-color);
+
+  @media screen and (min-width: 1024px) {
+    font-size: var(--font-md);
+  }
+}
+
+.footer-container__link {
+  color: var(--white);
 }
 </style>
