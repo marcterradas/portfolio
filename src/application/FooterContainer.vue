d513923be405407a60e9ca6e08bc4350cea0b275
Marc Terradas <terradas.marc@gmail.com>
2024-07-28 15:16:51 +0200

    Create FooterContainer.vue

---
+++ src/application/FooterContainer.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <div class="footer-container">
+    TODO: Footer...
+  </div>
+</template>
+
+<style>
+.footer-container {
+  display: flex;
+  justify-content: center;
+}
+</style>
