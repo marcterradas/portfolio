815809449a5c1ad04e5428906075091d9deae33c
Marc Terradas <terradas.marc@gmail.com>
2024-07-30 14:49:12 +0200

    Add languages

--- src/application/LanguagesContainer.vue
+++ src/application/LanguagesContainer.vue
@@ -1,5 +1,6 @@
 <script setup>
 import BaseTitleSection from '@/application/BaseTitleSection.vue'
+import BaseParagraph from '@/application/BaseParagraph.vue'
 </script>
 
 <template>
@@ -7,13 +8,30 @@ import BaseTitleSection from '@/application/BaseTitleSection.vue'
     <BaseTitleSection>
       {{ $t('languagesContainer.label.title') }}
     </BaseTitleSection>
+    <div class="languages-container__languages">
+      <BaseParagraph>
+        {{ $t('educationContainer.label.catalan') }}
+      </BaseParagraph>
+      <BaseParagraph>
+        {{ $t('educationContainer.label.spanish') }}
+      </BaseParagraph>
+      <BaseParagraph>
+        {{ $t('educationContainer.label.english') }}
+      </BaseParagraph>
+    </div>
   </div>
 </template>
 
 <style>
-.education-container {
+.languages-container {
   display: flex;
   flex-direction: column;
   gap:var(--spacer);
+}
+
+.languages-container__languages {
+  display: flex;
+  flex-direction: column;
+  gap: calc(var(--spacer)/2);
 }
 </style>
