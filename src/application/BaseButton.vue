87671d3aa93e14ec510dc0453d0537d1787e7529
Marc Terradas <terradas.marc@gmail.com>
2025-07-05 20:50:35 +0200

    Create BaseButton.vue

---
+++ src/application/BaseButton.vue
@@ -0,0 +1,63 @@
+<script setup>
+import { defineEmits, computed } from 'vue'
+
+const props = defineProps({
+  icon: {
+    type: String,
+    default: null,
+  },
+})
+
+defineEmits(['click'])
+
+const hasIcon = computed(() => !!props.icon)
+</script>
+
+<template>
+  <button
+    class="base-button"
+    @click="$emit('click')"
+  >
+    <img
+      v-if="hasIcon"
+      class="base-button__icon"
+      :src="icon"
+      alt="icon"
+    >
+    <slot />
+  </button>
+</template>
+
+<style>
+.base-button {
+  align-items: center;
+  background-color: var(--black);
+  border: var(--default-border) solid var(--dark-gray);
+  border-radius: var(--default-border-radius);
+  color: var(--white);
+  cursor: pointer;
+  display: flex;
+  font-size: var(--font-sm);
+  font-weight: bold;
+  gap: calc(var(--spacer)/4);
+  padding: calc(var(--spacer)/3);
+  text-decoration: none;
+  transition: var(--transition-background);
+  width: fit-content;
+}
+
+.base-button:hover {
+  background-color: var(--black-lightest);
+}
+
+.base-button__icon {
+  width: var(--default-size);
+  height: var(--default-size);
+}
+
+@media screen and (min-width: 1024px) {
+  .base-button {
+    font-size: var(--font-md);
+  }
+}
+</style>
