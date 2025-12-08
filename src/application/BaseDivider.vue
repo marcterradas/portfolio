4d77717482f8f90396c0cf91f36263c1ed8bf049
Marc Terradas <terradas.marc@gmail.com>
2025-12-08 18:50:47 +0100

    Add BaseDivider

---
+++ src/application/BaseDivider.vue
@@ -0,0 +1,19 @@
+<template>
+  <hr class="base-divider">
+</template>
+
+<style scoped>
+  .base-divider {
+      border: none;
+      height: 1px;
+      width: 100%;
+      background-color: var(--text-color);
+      opacity: 0.1;
+  }
+
+  @media print {
+      .base-divider {
+          display: none;
+      }
+  }
+</style>
